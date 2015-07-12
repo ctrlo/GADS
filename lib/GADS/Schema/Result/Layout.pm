cecde1f3101576a770f2db2c351417db8d715dac
Andrew Beverley <a.beverley@ctrlo.com>
2015-07-12 12:03:56 +0100

    Differences due to different dbic-dump output

--- lib/GADS/Schema/Result/Layout.pm
+++ lib/GADS/Schema/Result/Layout.pm
@@ -363,21 +363,6 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
-=head2 graph_x_axes
-
-Type: has_many
-
-Related object: L<GADS::Schema::Result::Graph>
-
-=cut
-
-__PACKAGE__->has_many(
-  "graph_x_axes",
-  "GADS::Schema::Result::Graph",
-  { "foreign.x_axis" => "self.id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
-
 =head2 graph_y_axes
 
 Type: has_many
@@ -390,6 +375,21 @@ __PACKAGE__->has_many(
   "graph_y_axes",
   "GADS::Schema::Result::Graph",
   { "foreign.y_axis" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+=head2 graphs_x_axis
+
+Type: has_many
+
+Related object: L<GADS::Schema::Result::Graph>
+
+=cut
+
+__PACKAGE__->has_many(
+  "graphs_x_axis",
+  "GADS::Schema::Result::Graph",
+  { "foreign.x_axis" => "self.id" },
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
@@ -574,8 +574,8 @@ __PACKAGE__->has_many(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07039 @ 2015-05-31 15:15:57
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:T3YPLxyoCM/bSitbwY9+hg
+# Created by DBIx::Class::Schema::Loader v0.07039 @ 2015-07-12 11:40:47
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:lhZjRG/+Ke8ONtSG/HHlVQ
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
