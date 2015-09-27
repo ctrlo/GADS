b182a13dff663761cf4878c3ebb8265add359c19
Andrew Beverley <a.beverley@ctrlo.com>
2015-09-27 20:41:05 +0100

    Add workaround for tables not being quoted in data seeding

--- share/migrations/_common/deploy/1/001-base.pl
+++ share/migrations/_common/deploy/1/001-base.pl
@@ -5,6 +5,10 @@ use DBIx::Class::Migration::RunScript;
 migrate {
 
     my $schema = shift->schema;
+    # dbic_connect_attrs is ignored, so quote_names needs to be forced
+    $schema->storage->connect_info(
+        [sub {$schema->storage->dbh}, { quote_names => 1 }]
+    );
 
     my $permission_rs = $schema->resultset('Permission');
 
