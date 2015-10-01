b72fc82ab7fed22bec25ba2be6147290cc78cbec
Andy Beverley <a.beverley@ctrlo.com>
2015-10-01 13:18:13 +0100

    Fix all column graphs causing exception

--- lib/GADS/Graph/Data.pm
+++ lib/GADS/Graph/Data.pm
@@ -191,9 +191,11 @@ sub _build_data
     my $view    = $self->records->view;
     # All the x values from the records. May only be one, or may be lots if
     # not defined in the graph
-    my @x       = ($x_axis)
-        || ($view && $self->records->layout->view($view->id, user_can_read => 1))
-        || $layout->all(user_can_read => 1);
+    my @x = $x_axis
+        ? ($x_axis)
+        : $view
+        ? $self->records->layout->view($view->id, user_can_read => 1)
+        : $layout->all(user_can_read => 1);
 
     my %xy_values; my %y_group_values;
     my ($datemin, $datemax);
