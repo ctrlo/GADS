3fb8499b05fe4f28f4c87d63b0b5b533c406f8dd
Andy Beverley <a.beverley@ctrlo.com>
2015-11-17 14:45:39 +0000

    Update dbic.sh script to ignore dbic-migration table

--- dbic.sh
+++ dbic.sh
@@ -3,5 +3,5 @@
 echo Using database sds. Please enter the root password of the mysql database:
 read PASSWORD
 
-dbicdump -o dump_directory=./lib -o components='["InflateColumn::DateTime"]' GADS::Schema 'dbi:mysql:dbname=sds' root $PASSWORD
+dbicdump -o dump_directory=./lib -o components='["InflateColumn::DateTime"]' -o exclude=dbix_class_deploymenthandler_versions GADS::Schema 'dbi:mysql:dbname=gads' root $PASSWORD
 
