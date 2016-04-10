c94de6cc32bf48558b040512a28c11e0ed878c94
Andy Beverley <a.beverley@ctrlo.com>
2016-04-10 17:01:55 +0100

    Trim whitespace at beginning of value

--- bin/onboard.pl
+++ bin/onboard.pl
@@ -194,6 +194,7 @@ while (my $row = $csv->getline($fh))
     {
         my $f = $fields[$col_count];
         $col =~ s/\s+$//;
+        $col =~ s/^\s+//;
         #say STDOUT "Going to process $col into field $f->{name}";
         if ($f->type eq "enum" || $f->type eq "tree" || $f->type eq "person")
         {
