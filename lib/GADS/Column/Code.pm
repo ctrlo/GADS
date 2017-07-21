2291cfce432708dacdf97e7823d0494b8295c2fe
Andy Beverley <a.beverley@ctrlo.com>
2017-07-21 17:37:39 +0100

    Add all permitted Lua functions

--- lib/GADS/Column/Code.pm
+++ lib/GADS/Column/Code.pm
@@ -29,11 +29,72 @@ use Inline 'Lua' => q{
         env["vars"] = vars
 
         env["ipairs"] = ipairs
-        env["math"] = { floor = math.floor, min = math.min, max = math.max, abs = math.abs }
-        env["os"] = { time = os.time, date = os.date }
+        env["math"] = {
+            abs = math.abs,
+            acos = math.acos,
+            asin = math.asin,
+            atan = math.atan,
+            atan2 = math.atan2,
+            ceil = math.ceil,
+            cos = math.cos,
+            cosh = math.cosh,
+            deg = math.deg,
+            exp = math.exp,
+            floor = math.floor,
+            fmod = math.fmod,
+            frexp = math.frexp,
+            huge = math.huge,
+            ldexp = math.ldexp,
+            log = math.log,
+            log10 = math.log10,
+            max = math.max,
+            min = math.min,
+            modf = math.modf,
+            pi = math.pi,
+            pow = math.pow,
+            rad = math.rad,
+            random = math.random,
+            sin = math.sin,
+            sinh = math.sinh,
+            sqrt = math.sqrt,
+            tan = math.tan,
+            tanh = math.tanh
+        }
+        env["next"] = next
+        env["os"] = {
+            clock = os.clock,
+            date = os.date,
+            difftime = os.difftime,
+            time = os.time
+        }
         env["pairs"] = pairs
-        env["table"] = { sort = table.sort, insert = table.insert }
+        env["pcall"] = pcall
+        env["select"] = select
+        env["string"] = {
+            byte = string.byte,
+            char = string.char,
+            find = string.find,
+            format = string.format,
+            gmatch = string.gmatch,
+            gsub = string.gsub,
+            len = string.len,
+            lower = string.lower,
+            match = string.match,
+            rep = string.rep,
+            reverse = string.reverse,
+            sub = string.sub,
+            upper = string.upper
+        }
+        env["table"] = {
+            insert = table.insert,
+            maxn = table.maxn,
+            remove = table.remove,
+            sort = table.sort
+        }
         env["tonumber"] = tonumber
+        env["tostring"] = tostring
+        env["type"] = type
+        env["unpack"] = unpack
 
         func, err = load(string, nil, 't', env)
         ret = {}
