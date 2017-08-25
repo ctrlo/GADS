9ca6189cec45deaafa14381a0f7d747a46d20fc4
Job van Achterberg <mail@3ode.nl>
2017-08-25 17:15:32 +0100

    Properly handle permission adding and removing

--- public/js/linkspace.js
+++ public/js/linkspace.js
@@ -1,33 +1,33 @@
+var positionDisclosure = function (offsetTop, offsetLeft, triggerHeight) {
+    var $disclosure = this;
+
+    var left = (offsetLeft) + 'px';
+    var top  = (offsetTop + triggerHeight ) + 'px';
+
+    $disclosure.css({
+        'left': left,
+        'top' : top
+    });
+};
+
+var toggleDisclosure = function () {
+    var $trigger = $(this);
+    var $disclosure = $trigger.next('.expandable');
+
+    var offset = $trigger.position();
+    positionDisclosure.call(
+        $disclosure, offset.top, offset.left, $trigger.height()
+    );
+
+    var currentlyExpanded = $trigger.attr('aria-expanded') === 'true';
+    $trigger.attr('aria-expanded', !currentlyExpanded);
+    
+    $disclosure.toggleClass('expanded', !currentlyExpanded);
+
+    $trigger.trigger((currentlyExpanded ? 'collapse' : 'expand'), $disclosure);
+};
+
 var setupDisclosureWidgets = function () {
-    var positionDisclosure = function (offsetTop, offsetLeft, triggerHeight) {
-        var $disclosure = this;
-
-        var left = (offsetLeft) + 'px';
-        var top  = (offsetTop + triggerHeight ) + 'px';
-
-        $disclosure.css({
-            'left': left,
-            'top' : top
-        });
-    };
-
-    var toggleDisclosure = function () {
-        var $trigger = $(this);
-        var $disclosure = $trigger.next('.expandable');
-
-        var offset = $trigger.position();
-        positionDisclosure.call(
-            $disclosure, offset.top, offset.left, $trigger.height()
-        );
-
-        var currentlyExpanded = $trigger.attr('aria-expanded') === 'true';
-        $trigger.attr('aria-expanded', !currentlyExpanded);
-        
-        $disclosure.toggleClass('expanded', !currentlyExpanded);
-
-        $trigger.trigger((currentlyExpanded ? 'collapse' : 'expand'), $disclosure);
-    };
-
     $('.trigger[aria-expanded]').on('click', toggleDisclosure);
 }
 
@@ -52,14 +52,34 @@ var Linkspace = {
 
 Linkspace.layout = function () {
     Linkspace.debug('Layout JS firing');
+
     $('#show-advanced-field-settings').on('expand', function (event, target) {
         $(target).find('h3').focus();
         $(this).remove();
     });
-    $('#current-permissions .permission').each(function () {
+
+    var $config = $('#permission-configuration');
+    var $rule = $('.permission-rule');
+
+    var $ruleTemplate = $('#permission-rule-template');
+    var $cancelRuleButton = $rule.find('button.cancel-permission');
+    var $addRuleButton = $rule.find('button.add-permission'); 
+
+    var closePermissionConfig = function () {
+        $config.find('input').each(function () {
+            $(this).prop('checked', false);
+        });
+        $config.attr('hidden', '');
+        $('#configure-permissions').removeAttr('hidden').focus();
+    };
+
+    var handlePermissionChange = function () {
         var $permission = $(this);
-        var $editButton  = $permission.find('button.edit');
-        var $okButton    = $permission.find('button.ok');
+        var groupId = $permission.data('group-id');
+
+        var $editButton   = $permission.find('button.edit');
+        var $deleteButton = $permission.find('button.delete');
+        var $okButton     = $permission.find('button.ok');
 
         $permission.find('input').on('change', function () {
             var pClass = 'permission-' + $(this).data('permission-class');
@@ -75,9 +95,14 @@ Linkspace.layout = function () {
             });
         });
 
-        $editButton.on('expand', function (event, target) {
+        $editButton.on('expand', function (event) {
             $permission.addClass('edit');
             $permission.find('h7').focus();
+        });
+
+        $deleteButton.on('click', function (event) {
+            $('#permissions').removeClass('permission-group-' + groupId);
+            $permission.remove();
         });
 
         $okButton.on('click', function (event) {
@@ -85,7 +110,54 @@ Linkspace.layout = function () {
             $okButton.parent().removeClass('expanded');
             $editButton.attr('aria-expanded', false).focus();
         });
+    };
+
+    $cancelRuleButton.on('click', closePermissionConfig);
+
+    $addRuleButton.on('click', function () {
+        var $newRule = $($ruleTemplate.html());
+        var $currentPermissions = $('#current-permissions ul');
+        var $selectedGroup = $config.find('option:selected');
+
+        var groupId = $selectedGroup.val();
+
+        $config.find('input').each(function () {
+            var $input = $(this);
+            var state = $input.prop('checked');
+
+            if (state) {
+                $newRule.addClass(
+                    'permission-' +
+                    $input.data('permission-class').replace(/_/g,'-')
+                );
+            }
+
+            $newRule.find('input#' + $input.attr('id'))
+                .prop('checked', state)
+                .attr('id', $input.attr('id') + groupId)
+                .attr('name', $input.attr('name') + groupId)
+                .next('label').attr('for', $input.attr('id'));
+        });
+
+        $newRule.appendTo($currentPermissions);
+        $newRule.attr('data-group-id', groupId);
+
+        $('#permissions').addClass('permission-group-' + groupId);
+        $newRule.find('.group-name').text($selectedGroup.text());
+
+        $newRule.find('button.edit').on('click', toggleDisclosure);
+
+        handlePermissionChange.call($newRule);
+        closePermissionConfig();
     });
+
+    $('#configure-permissions').on('click', function () {
+        $(this).attr('hidden', '');
+        $('#permission-configuration').removeAttr('hidden');
+        $(this).parent().find('h4').focus();
+    }); 
+
+    $('#current-permissions .permission').each(handlePermissionChange);
 };
 
 $(Linkspace.init);
