b4992b56225621014e2dcfed66ff29dc8e2685e5
Andy Beverley <a.beverley@ctrlo.com>
2017-10-31 08:16:28 +0000

    Allow blank organisation

--- lib/GADS/Schema/Result/User.pm
+++ lib/GADS/Schema/Result/User.pm
@@ -717,7 +717,7 @@ sub update_user
         or error __"Surname must be less than 128 characters";
     length $params{telephone} <= 128
         or error __"Telephone must be less than 128 characters";
-    $params{organisation} =~ /^[0-9]+$/
+    !defined $params{organisation} || $params{organisation} =~ /^[0-9]+$/
         or error __x"Invalid organisation {id}", id => $params{organisation};
 
     my $values = {
