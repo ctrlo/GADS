83831dd428411e636b5c7c4851eb8fb6975d7a07
Job van Achterberg <mail@3ode.nl>
2017-12-20 12:53:23 +0000

    Handle comma-separation and <blank> placeholder toggling

--- public/js/linkspace.js
+++ public/js/linkspace.js
@@ -56,6 +56,56 @@ var setupLessMoreWidgets = function () {
     $widgets.each(convert);
 };
 
+var MultiSelectWidget = function () {
+    var connect = function (update) {
+        return function () {
+            var $item = $(this);
+            var itemId = $item.data('list-item');
+            var $associated = $('#' + itemId);
+            $associated.on('change', function () {
+                if ($(this).prop('checked')) {
+                    $item.prop('hidden', false);
+                } else {
+                    $item.prop('hidden', true);
+                }
+                update();
+            });
+        };
+    };
+ 
+    var onTriggerClick = function ($target) {
+        return function (event) {
+            var isCurrentlyExpanded = $(this).attr('aria-expanded') === 'true';
+            var willExpandNext = !isCurrentlyExpanded;
+
+            if (willExpandNext) {
+                $target.prop('hidden', false);
+                $(this).attr('aria-expanded', 'true');
+            } else {
+                $(this).attr('aria-expanded', 'false');
+                $target.prop('hidden', true);
+            }
+        }
+    };
+    
+    var $trigger = this.find('button');
+    var $current = this.find('.current');
+    var $target  = this.find('#' + $trigger.attr('aria-controls'));
+    var $currentItems = $current.children();
+
+    var updateState = function () {
+        var $visible = $current.children('[data-list-item]:not([hidden])');
+        $current.toggleClass('empty', $visible.length === 0);
+        $visible.each(function (index) {
+            $(this).toggleClass('comma-separated', index < $visible.length-1); 
+        });
+    };
+
+    updateState();
+    $currentItems.each(connect(updateState));
+    $trigger.on('click', onTriggerClick($target));   
+};
+
 var positionDisclosure = function (offsetTop, offsetLeft, triggerHeight) {
     var $disclosure = this;
 
