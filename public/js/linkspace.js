ff752fa52d2a0b30db17999d09980715d3096a44
Job van Achterberg <mail@3ode.nl>
2018-01-31 13:19:40 +0000

    Tab mouse and keyboard navigation

--- public/js/linkspace.js
+++ public/js/linkspace.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var setupLessMoreWidgets = function () {
     var MAX_HEIGHT = 100;
 
@@ -108,24 +110,81 @@ var runPageSpecificCode = function () {
 };
 
 var Linkspace = {
+    constants: {
+        ARROW_LEFT: 37,
+        ARROW_RIGHT: 39
+    },
+
     init: function () {
         console.clear();
         setupLessMoreWidgets();
         setupDisclosureWidgets();
         runPageSpecificCode();
     },
+
     debug: function (msg) {
         console.debug('[LINKSPACE]', msg);
+    },
+
+    TabPanel: function () {
+        var $this = $(this);
+        var $tabs = $this.find('[role="tab"]');
+        var $panels = $this.find('[role="tabpanel"]');
+
+        var indexedTabs = [];
+
+        $tabs.each(function (i) {
+            indexedTabs[i] = $(this);
+            $(this).data('index', i);
+        });
+
+        var selectTab = function (e) {
+            if (e) { e.preventDefault(); }
+            var $thisTab = $(this);
+
+            if ($thisTab.attr('aria-selected') === 'true') { return false; }
+
+            var $thisPanel = $panels.filter($thisTab.attr('href'));
+
+            var $activeTab = $tabs.filter('[aria-selected="true"]');
+            var $activePanel = $panels.filter('.active');
+
+            $activeTab.attr('aria-selected', false);
+            $activePanel.removeClass('active');
+
+            $thisTab.attr('aria-selected', true);
+            $thisPanel.addClass('active');
+
+            $thisTab.attr('tabindex', '0');
+            $tabs.filter('[aria-selected="false"]').attr('tabindex', '-1');
+        };
+
+        var moveTab = function (e) {
+            var $thisTab = $(this);
+            var index = $thisTab.data('index');
+            var k = e.keyCode;
+            var left = Linkspace.constants.ARROW_LEFT, right = Linkspace.constants.ARROW_RIGHT;
+            if ([left, right].indexOf(k) < 0) { return; }
+            var $nextTab;
+            if (
+                (k === left  && ($nextTab = indexedTabs[index-1])) ||
+                (k === right && ($nextTab = indexedTabs[index+1]))
+            ) {
+                selectTab.call($nextTab);
+                $nextTab.focus();
+            }
+        };
+
+        $tabs.on('click', selectTab);
+        $tabs.on('keyup', moveTab);
+        $tabs.filter('[aria-selected="false"]').attr('tabindex', '-1');
     }
 };
 
 Linkspace.layout = function () {
     Linkspace.debug('Layout JS firing');
 
-    $('#show-advanced-field-settings').on('expand', function (event, target) {
-        $(target).find('h3').focus();
-        $(this).remove();
-    });
+    $('.tab-interface').each(Linkspace.TabPanel);
 
     var $config = $('#permission-configuration');
     var $rule = $('.permission-rule');
