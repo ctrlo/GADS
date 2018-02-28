12e7d8ffba4f17bf6740662d6383e0584d5ffa40
Job van Achterberg <mail@3ode.nl>
2018-02-28 15:09:40 +0000

    Support single/multi value selection widget

--- public/js/linkspace.js
+++ public/js/linkspace.js
@@ -1,18 +1,31 @@
 'use strict';
 
-var MultiSelectWidget = function () {
-    var connect = function (update) {
+var SelectWidget = function (multi) {
+
+    var connectMulti = function (update) {
         return function () {
             var $item = $(this);
             var itemId = $item.data('list-item');
             var $associated = $('#' + itemId);
             $associated.on('change', function () {
+                console.log(this.id, $(this).prop('checked'));
                 if ($(this).prop('checked')) {
                     $item.prop('hidden', false);
                 } else {
                     $item.prop('hidden', true);
                 }
                 update();
+            });
+        };
+    };
+
+    var connectSingle = function (update) {
+        return function () {
+            var $item = $(this);
+            var itemId = $item.data('list-item');
+            var $associated = $('#' + itemId);
+            $associated.on('change', function () {
+                update($item);
             });
         };
     };
@@ -24,36 +37,52 @@ var MultiSelectWidget = function () {
 
             if (willExpandNext) {
                 $target.prop('hidden', false);
-                $(this).attr('aria-expanded', 'true');
+                $(this).attr('aria-expanded', true);
             } else {
-                $(this).attr('aria-expanded', 'false');
+                $(this).attr('aria-expanded', false);
                 $target.prop('hidden', true);
             }
         }
     };
     
-    var $trigger = this.find('button');
+    var $trigger = this.find('button.change');
     var $current = this.find('.current');
+    var $availableItems = this.find('.available').children();
     var $target  = this.find('#' + $trigger.attr('aria-controls'));
     var $currentItems = $current.children();
 
     var updateState = function () {
         var $visible = $current.children('[data-list-item]:not([hidden])');
         $current.toggleClass('empty', $visible.length === 0);
-        $visible.each(function (index) {
-            $(this).toggleClass('comma-separated', index < $visible.length-1); 
-        });
+        if (multi) {
+            $visible.each(function (index) {
+                $(this).toggleClass('comma-separated', index < $visible.length-1); 
+            });
+        }
     };
 
     updateState();
-    $currentItems.each(connect(updateState));
+    
+    if (multi) {
+        $currentItems.each(connectMulti(updateState));
+    } else {
+        $currentItems.each(connectSingle(function ($connected) {
+            $currentItems.each(function () {
+                $(this).prop('hidden', true);
+            });
+
+            $connected.prop('hidden', false);
+        }));
+    }
+
     $trigger.on('click', onTriggerClick($target));   
 };
 
-var setupMultiSelectWidgets = function () {
-    var $nodes = $('.select-widget.multi');
+var setupSelectWidgets = function () {
+    var $nodes = $('.select-widget');
     $nodes.each(function () {
-        MultiSelectWidget.call($(this));
+        var multi = $(this).hasClass('multi');
+        SelectWidget.call($(this), multi);
     });
 };
 
@@ -173,10 +202,9 @@ var Linkspace = {
     },
 
     init: function () {
-        console.clear();
         setupLessMoreWidgets();
         setupDisclosureWidgets();
-        setupMultiSelectWidgets();
+        setupSelectWidgets();
         runPageSpecificCode();
     },
 
