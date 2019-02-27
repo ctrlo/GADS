caec253f98a518c0605461905cdbaa8d050639b6
Thomas Hartman <thomas@digitpaint.nl>
2019-02-27 17:53:25 +0100

    Add progress bar to file uploads

--- public/js/linkspace.js
+++ public/js/linkspace.js
@@ -608,11 +608,35 @@ var setupFileUpload = function (context) {
         var $ul = $el.find("ul");
         var url = $el.data("fileupload-url");
         var field = $el.data("field");
+        var $progressBarContainer = $el.find('.progress-bar__container');
+        var $progressBarProgress = $el.find('.progress-bar__progress');
+        var $progressBarPercentage = $el.find('.progress-bar__percentage')
 
         $el.fileupload({
             dataType: 'json',
             url: url,
             paramName: "file",
+
+            submit: function (e, data) {
+                $progressBarContainer.css('display', 'block');
+                $progressBarPercentage.html("0%");
+                $progressBarProgress.css('width', '0%');
+            },
+            progress: function (e,data) {
+                if (!$el.data("multivalue")) {
+                    var $uploadProgression = Math.round(data.loaded / data.total * 10000)/100 + '%';
+                    $progressBarPercentage.html($uploadProgression);
+                    $progressBarProgress.css('width', $uploadProgression);
+                }
+            },
+            progressall: function (e, data) {
+                if ($el.data("multivalue")) {
+                    var $uploadProgression = Math.round(data.loaded / data.total * 10000)/100 + '%';
+                    $progressBarPercentage.html($uploadProgression);
+                    $progressBarProgress.css('width', $uploadProgression);
+                }
+
+            },
             done: function (e, data) {
                 if (!$el.data("multivalue")) {
                     $ul.empty();
