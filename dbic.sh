32ebc884f7dfd2764072add1c75fcf6b2a45e050
Mark Overmeer <mark@overmeer.net>
2019-05-14 10:54:14 +0200

    improve dbic.sh (needs more work)

--- dbic.sh
+++ dbic.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+#XXX Please describe the application of this script.
+#XXX Move to bin/ or devel/ with a useful name
 
-echo Using database sds. Please enter the root password of the mysql database:
-read PASSWORD
+echo "Using database sds."   #XXX sds?
+read -p "root password of the mysql database: " -r -s PASSWORD
+echo
 
-dbicdump -o dump_directory=./lib -o components='["InflateColumn::DateTime"]' -o exclude=dbix_class_deploymenthandler_versions GADS::Schema 'dbi:mysql:dbname=gads' root $PASSWORD
+if [ -z "$PASSWORD" ]
+then echo "No password. Stopped." >&2
+     exit 1
+fi
+
+dbicdump \
+    -o dump_directory=./lib \
+    -o components='["InflateColumn::DateTime"]' \
+    -o exclude=dbix_class_deploymenthandler_versions \
+    GADS::Schema 'dbi:mysql:dbname=gads' \
+    root "$PASSWORD"
 
