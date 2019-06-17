e4481643d4c175ba4228f1bd59f343b1baa99c21
Andy Beverley <a.beverley@ctrlo.com>
2019-06-17 08:03:12 +0100

    Fix failing tests

--- lib/GADS/Schema/Result/Record.pm
+++ lib/GADS/Schema/Result/Record.pm
@@ -404,8 +404,17 @@ __PACKAGE__->might_have(
             "$args->{foreign_alias}.current_id"  => { -ident => "$args->{self_alias}.current_id" },
             # Changed from using "id" as the key to see which record is later,
             # as after an import the IDs may be in a different order to that in
-            # which the records were created
-            "$args->{foreign_alias}.created"     => { '>' => \"$args->{self_alias}.created" },
+            # which the records were created. If the created date is the same,
+            # use the IDs (primarily for tests, which sometimes have fixed times).
+            -or => [
+                {
+                    "$args->{foreign_alias}.created" => { '>' => \"$args->{self_alias}.created" },
+                },
+                {
+                    "$args->{foreign_alias}.created" => { '=' => \"$args->{self_alias}.created" },
+                    "$args->{foreign_alias}.id"      => { '>' => \"$args->{self_alias}.id" },
+                },
+            ],
             "$args->{foreign_alias}.approval"    => 0,
         };
         $return->{"$args->{foreign_alias}.created"} = { '<' => $REWIND }
