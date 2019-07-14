49915515d8ccd49aaed0176fa74e5925b8b512c6
Andy Beverley <a.beverley@ctrlo.com>
2019-07-14 23:35:40 +0100

    Fix empty multi-select fields not being tested for display conditions

--- public/js/linkspace.js
+++ public/js/linkspace.js
@@ -588,36 +588,43 @@ var getFieldValues = function ($depends) {
     // If a field is not shown then treat it as a blank value (e.g. if fields
     // are in a hierarchy and the top one is not shown
     if ($depends.css('display') == 'none') {
-        return '';
+        return [''];
     }
 
     var type = $depends.data('column-type');
 
+    var values = [];
+
     if (type === 'enum' || type === 'curval') {
         var $visible = $depends.find('.select-widget .current [data-list-item]:not([hidden])');
-        var items = [];
         $visible.each(function () {
             var item = $(this).hasClass("current__blank") ? "" : $(this).text();
-            items.push(item)
+            values.push(item)
         });
-        return items;
     } else if (type === 'person') {
-        return [$depends.find('option:selected').text()];
+        values = [$depends.find('option:selected').text()];
     } else if (type === 'tree') {
         // get the hidden fields of the control - their textual value is located in a dat field
-        var items = [];
-        $depends.find('.selected-tree-value').each(function() { items.push($(this).data('text-value')) });
-        return items;
+        $depends.find('.selected-tree-value').each(function() { values.push($(this).data('text-value')) });
     } else if (type === 'daterange') {
         var $f = $depends.find('.form-control');
-        var dates = $f.map(function() {
+        values = $f.map(function() {
             return $(this).val();
         }).get().join(' to ');
-        return [dates];
     } else {
         var $f = $depends.find('.form-control');
-        return [$f.val()];
+        values = [$f.val()];
     }
+
+    // A multi-select field with no values selected should be the same as a
+    // single-select with no values. Ensure that both are returned as a single
+    // empty string value. This is important for display_condition testing, so
+    // that at least one value is tested, even if it's empty
+    if (values.length == 0) {
+        values = [''];
+    }
+
+    return values;
 };
 
 var setupFileUpload = function (context) {
