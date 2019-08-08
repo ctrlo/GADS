381aef88870cdee8866747e9d98a0d1b71e8c69f
Ronald Wind <ronald@digitpaint.nl>
2019-08-08 14:48:37 +0000

    Add webpack to compile js and ts

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { resolve } = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'js/general.js',
+    path: resolve(__dirname, './public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.scss'],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/general.css',
+    })
+  ],
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000
+  }
+};
