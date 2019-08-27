c9ac25f37f8330df5eb55b432e89b36836525ee5
Ronald Wind <ronald@digitpaint.nl>
2019-08-27 09:27:11 +0000

    Make the the widget edit button is clickable

--- src/app.tsx
+++ src/app.tsx
@@ -165,10 +165,10 @@ class App extends React.Component<any, any> {
   generateDOM = () => (
     this.state.widgets.map(widget => (
       <div key={widget.config.i} className="ld-widget-container">
+        <div dangerouslySetInnerHTML={{ __html: widget.html }} />
         <div className="ld-hover-region ld-hover-region__edit">
           <button className="ld-edit-button btn btn-sm btn-primary" onClick={this.onEditClick(widget.config.i)}>Edit</button>
         </div>
-        <div dangerouslySetInnerHTML={{ __html: widget.html }} />
         <div className="ld-hover-region ld-hover-region__handle" />
       </div>
     ))
