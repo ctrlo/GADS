e1d8f4388a63c7a829bed75b500904b928bc6c04
Andy Beverley <a.beverley@ctrlo.com>
2019-08-29 23:57:35 +0100

    Fix closure of widget modal on success

--- src/app.tsx
+++ src/app.tsx
@@ -112,7 +112,7 @@ class App extends React.Component<any, any> {
 
     const form = serialize(formEl);
     const result = await this.props.api.saveWidget(formEl.getAttribute("action"), form);
-    if (!result.error) {
+    if (result.is_error) {
       this.setState({ editError: result.message });
       return;
     }
