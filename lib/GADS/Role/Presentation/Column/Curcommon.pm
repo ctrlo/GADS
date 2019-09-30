cb20968841d3796a9efdf5d4d558367a3a557711
Andy Beverley <a.beverley@ctrlo.com>
2019-09-30 11:09:57 +0100

    Fix add button not showing for curval

--- lib/GADS/Role/Presentation/Column/Curcommon.pm
+++ lib/GADS/Role/Presentation/Column/Curcommon.pm
@@ -5,8 +5,9 @@ use Moo::Role;
 sub after_presentation
 {   my ($self, $return) = @_;
 
-    $return->{layout_parent} = $self->layout_parent;
-    $return->{value_selector} = $self->value_selector;
+    $return->{layout_parent}   = $self->layout_parent;
+    $return->{value_selector}  = $self->value_selector;
+    $return->{show_add}        = $self->show_add;
     $return->{filtered_values} = $self->filtered_values;
 }
 
