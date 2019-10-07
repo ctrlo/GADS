4288c6b68c1dc0a370c29ce5c5721430db4b9d23
Andy Beverley <a.beverley@ctrlo.com>
2019-10-07 01:25:27 +0100

    Fix filtered curvals not working

--- lib/GADS/Role/Presentation/Column/Curcommon.pm
+++ lib/GADS/Role/Presentation/Column/Curcommon.pm
@@ -5,11 +5,12 @@ use Moo::Role;
 sub after_presentation
 {   my ($self, $return) = @_;
 
-    $return->{layout_parent}   = $self->layout_parent;
-    $return->{value_selector}  = $self->value_selector;
-    $return->{show_add}        = $self->show_add;
-    $return->{has_subvals}     = $self->has_subvals;
-    $return->{filtered_values} = $self->filtered_values;
+    $return->{layout_parent}      = $self->layout_parent;
+    $return->{value_selector}     = $self->value_selector;
+    $return->{show_add}           = $self->show_add;
+    $return->{has_subvals}        = $self->has_subvals;
+    $return->{filtered_values}    = $self->filtered_values;
+    $return->{data_filter_fields} = $self->data_filter_fields;
 }
 
 1;
