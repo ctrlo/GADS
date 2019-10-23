1ca936f2ae0d1c223efd58e4a9df31a2b8fb70ca
Andy Beverley <a.beverley@ctrlo.com>
2019-10-23 16:40:43 +0100

    Fix filtered graphs not being selected

--- public/js/linkspace.js
+++ public/js/linkspace.js
@@ -2129,6 +2129,19 @@ Linkspace.metric = function () {
 
 Linkspace.graphs = function (context) {
     setupDataTables(context);
+    // When a search is entered in a datatables table, selected graphs that are
+    // filtered will not be submitted. Therefore, find all selected values and
+    // add them to the form
+    $('#submit').on('click', function(e){
+        var t = $('.dtable').DataTable().column(0).nodes().to$().each(function() {
+            var $cell = $(this);
+            var $checkbox = $cell.find('input');
+            if ($checkbox.is(':checked')) {
+                $('<input type="hidden" name="graphs">').val($checkbox.val()).appendTo('form');
+            }
+        });
+    });
+
 };
 
 Linkspace.graph = function (context) {
