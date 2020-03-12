d4ea33eaa64f678408a16948ad8031189de1237d
Andy Beverley <a.beverley@ctrlo.com>
2020-03-12 10:30:34 +0000

    Update example record update script

--- bin/update-many-records.pl
+++ bin/update-many-records.pl
@@ -18,46 +18,66 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =cut
 
-# Example script for updating values across all records based on another
-# field's value
+# Example script for bulk updating records. Edit as required.
 
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 
+use GADS::DB;
+use GADS::Instances;
+use GADS::Layout;
 use Dancer2;
 use Dancer2::Plugin::DBIC;
-use Data::Dumper;
-use GADS::DB;
-use GADS::Layout;
-use GADS::Record;
-use Log::Report;
-use Text::CSV;
-use Getopt::Long;
+use Dancer2::Plugin::LogReport mode => 'NORMAL';
 
 GADS::DB->setup(schema);
+GADS::Config->instance(
+    config => config,
+);
 
-# All valid values
-my @valid = rset('Enumval')->search({ layout_id => 42 })->all;
+my $user_id = # XXX
+my $user    = schema->resultset('User')->find($user_id);
 
-my @text = rset('String')->search({ layout_id => 74 })->all;
+my $layout = GADS::Layout->new(
+    user        => $user,
+    schema      => schema,
+    instance_id => # XXX,
+);
+my $views      = GADS::Views->new(
+    user        => $user,
+    schema      => schema,
+    layout      => $layout,
+    instance_id => # XXX,
+);
+my $view = $views->view(1); # XXX
+my $records = GADS::Records->new(
+    user   => $user,
+    view   => $view,
+    layout => $layout,
+    schema => schema,
+);
 
-foreach my $text (@text)
+my $guard = schema->txn_scope_guard;
+
+foreach my $record (@{$records->results})
 {
-    next unless $text->value;
-    $text->value =~ /([a-z][0-9]{4}[a-z])/i;
-    my $v = $1 or next;
-    if (my @exists = (grep { $_->value =~ /$v/i } @valid))
-    {
-        next if @exists > 1;
-        my $u = pop @exists;
-        my $record_id = $text->record_id;
-        say STDOUT "Going to change https://xxxx/history/$record_id to ".$u->id." (".$u->value.")";
-        my $update = rset('Enum')->search({ record_id => $record_id, layout_id => 42 });
-        say STDOUT "This will update ".$update->count." records";
-        $update->update({ value => $u->id });
-        say STDOUT "Going to clear value from https://xxxx/history/$record_id";
-        $update = rset('String')->search({ record_id => $record_id, layout_id => 74 });
-        say STDOUT "This will update ".$update->count." records";
-        $update->update({ value => undef });
-    }
+    my $latest = schema->resultset('Date')->search({
+        current_id => $record->current_id,
+        value      => { '!=' => undef },
+        layout_id  => # XXX,
+    },{
+        join => 'record',
+    })->get_column('id')->max;
+    my $val = schema->resultset('Date')->find($latest)->value;
+    schema->resultset('Date')->search({
+        'record.current_id' => $record->current_id,
+        'me.id'             => { '>' => $latest },
+        layout_id           => # XXX,
+    },{
+        join => 'record',
+    })->update({
+        value => $val,
+    });
 }
+
+$guard->commit;
