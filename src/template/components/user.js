b708a12e0e27f1a49b6bfe9cb5bc49ae13f3a5c3
Andy Beverley <a.beverley@ctrlo.com>
2020-03-30 13:50:21 +0100

    Fix double-initialisation of DataTable

--- src/template/components/user.js
+++ src/template/components/user.js
@@ -1,14 +1,5 @@
-const setupDataTable = (id, context) => {
-  const tableEl = $(id, context);
-  if (!tableEl.length) return;
-  tableEl.dataTable({
-    order: [[1, "asc"]]
-  });
-};
-
 const setup = context => {
-  setupDataTable("#user-table-active", context);
-  setupDataTable("#user-table-request", context);
+  // Placeholder for future functionality
 };
 
 export default setup;
