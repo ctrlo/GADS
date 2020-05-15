1cd7a9c3ef4ff5d5c92abdf1cbcbf5dabc5e916a
Ronald Wind <ronald@digitpaint.nl>
2020-05-15 16:23:03 +0000

    Don't use polyfills from babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
-import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/template/index.js',
@@ -24,7 +22,5 @@ export default {
         ],
       ],
     }),
-    resolve(),
-    commonjs(),
   ]
 };
--- src/template/index.js
+++ src/template/index.js
@@ -1,4 +1,134 @@
-import 'core-js/modules/es.object.keys.js';
+if (!Array.prototype.find) {
+  Array.prototype.find = function (predicate) {
+    // 1. Let O be ? ToObject(this value).
+    if (this == null) {
+      throw TypeError('"this" is null or not defined');
+    }
+
+    var o = Object(this);
+
+    // 2. Let len be ? ToLength(? Get(O, "length")).
+    var len = o.length >>> 0;
+
+    // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+    if (typeof predicate !== 'function') {
+      throw TypeError('predicate must be a function');
+    }
+
+    // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    var thisArg = arguments[1];
+
+    // 5. Let k be 0.
+    var k = 0;
+
+    // 6. Repeat, while k < len
+    while (k < len) {
+      // a. Let Pk be ! ToString(k).
+      // b. Let kValue be ? Get(O, Pk).
+      // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+      // d. If testResult is true, return kValue.
+      var kValue = o[k];
+      if (predicate.call(thisArg, kValue, k, o)) {
+        return kValue;
+      }
+      // e. Increase k by 1.
+      k++;
+    }
+
+    // 7. Return undefined.
+    return undefined;
+  };
+}
+
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function(searchElement, fromIndex) {
+    if (this == null) {
+      throw new TypeError('"this" is null or not defined');
+    }
+
+    // 1. Let O be ? ToObject(this value).
+    var o = Object(this);
+
+    // 2. Let len be ? ToLength(? Get(O, "length")).
+    var len = o.length >>> 0;
+
+    // 3. If len is 0, return false.
+    if (len === 0) {
+      return false;
+    }
+
+    // 4. Let n be ? ToInteger(fromIndex).
+    //    (If fromIndex is undefined, this step produces the value 0.)
+    var n = fromIndex | 0;
+
+    // 5. If n ≥ 0, then
+    //  a. Let k be n.
+    // 6. Else n < 0,
+    //  a. Let k be len + n.
+    //  b. If k < 0, let k be 0.
+    var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+    function sameValueZero(x, y) {
+      return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+    }
+
+    // 7. Repeat, while k < len
+    while (k < len) {
+      // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+      // b. If SameValueZero(searchElement, elementK) is true, return true.
+      if (sameValueZero(o[k], searchElement)) {
+        return true;
+      }
+      // c. Increase k by 1.
+      k++;
+    }
+
+    // 8. Return false
+    return false;
+  };
+}
+
+if (!Array.prototype.map) {
+  Array.prototype.map = function (fun /*, thisp */ ) {
+    if (this === void 0 || this === null) {
+      throw TypeError();
+    }
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function") {
+      throw TypeError();
+    }
+
+    var res = [];
+    res.length = len;
+    var thisp = arguments[1],
+      i;
+    for (i = 0; i < len; i++) {
+      if (i in t) {
+        res[i] = fun.call(thisp, t[i], i, t);
+      }
+    }
+
+    return res;
+  };
+}
+
+if (!Object.keys) {
+  Object.keys = function (o) {
+    if (o !== Object(o)) {
+      throw TypeError('Object.keys called on non-object');
+    }
+    var ret = [],
+      p;
+    for (p in o) {
+      if (Object.prototype.hasOwnProperty.call(o, p)) {
+        ret.push(p);
+      }
+    }
+    return ret;
+  };
+}
 
 //
 // setupFontAwesome
