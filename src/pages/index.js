24e4f4c6649e1333c67c4596559f016745fc2565
Andy Beverley <a.beverley@ctrlo.com>
2020-12-09 15:42:23 +0000

    Fix graphs on dashboard not working

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ const IndexPage = context => {
   $(document).ready(function() {
     $(".dashboard-graph", context).each(function() {
       var graph = $(this);
-      var graph_data = base64.decode(graph.data("plot-data"));
-      var options_in = base64.decode(graph.data("plot-options"));
+      var graph_data = graph.data("plot-data");
+      var options_in = graph.data("plot-options");
       do_plot_json(graph_data, options_in);
     });
     $(".visualization", context).each(function() {
