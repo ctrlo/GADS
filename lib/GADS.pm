1087ed5cca8bff4573c11dcfd3eb11bff88cad14
Erik Stolman <erik.stolman@digitpaint.nl>
2022-01-26 12:07:31 +0100

    Add Department management, add and edit pages

--- lib/GADS.pm
+++ lib/GADS.pm
@@ -940,7 +940,7 @@ any ['get', 'post'] => '/settings/organisation_overview/' => require_any_role [q
 
     if (my $delete_id = param('delete'))
     {
-        my $organisation      = schema->resultset('Organisation')->find($delete_id);
+        my $organisation = schema->resultset('Organisation')->find($delete_id);
 
         if (process( sub { $organisation->delete_organisation } ))
         {
@@ -1024,15 +1024,38 @@ any ['get', 'post'] => '/settings/organisation_edit/:id' => require_any_role [qw
     };
 };
 
-any ['get', 'post'] => '/department/?:id?' => require_any_role [qw/useradmin superadmin/] => sub {
+any ['get', 'post'] => '/settings/department_overview/' => require_any_role [qw/useradmin superadmin/] => sub {
+    my $department_name = lcfirst(var('site')->department_name);
 
-    my $id     = route_parameters->get('id');
+    if (my $delete_id = param('delete'))
+    {
+        my $department = schema->resultset('Department')->find($delete_id);
 
-    my $department = $id
-        ? schema->resultset('Department')->find($id)
-        : schema->resultset('Department')->new({});
+        if (process( sub { $department->delete_department } ))
+        {
+            return forwardHome(
+                { success => "The $department_name has been deleted successfully" }, 'settings/department_overview/' );
+        }
+    }
 
-    my $department_name = var('site')->department_name;
+    template 'layouts/page_overview_name_only' => {
+        page               => 'department',
+        body_class         => 'page',
+        container_class    => 'container-fluid',
+        main_class         => 'main col-lg-10',
+        page_title         => "Manage ${department_name}s",
+        page_description   => "In this window you can list the parts of the ${department_name} that you want to assign users to. You can update the existing items or add new ones. Changes in here will impact all users currently assigned if you delete or edit a value.",
+        table_column_label => "Name",
+        item_type          => $department_name,
+        add_path           => "settings/department_add",
+        edit_path          => "settings/department_edit",
+        items              => [schema->resultset('Department')->ordered],
+    };
+};
+
+any ['get', 'post'] => '/settings/department_add/' => require_any_role [qw/useradmin superadmin/] => sub {
+    my $department      = schema->resultset('Department')->new({});
+    my $department_name = lcfirst(var('site')->department_name);
 
     if (body_parameters->get('submit'))
     {
@@ -1040,25 +1063,130 @@ any ['get', 'post'] => '/department/?:id?' => require_any_role [qw/useradmin sup
         if (process( sub { $department->insert_or_update } ))
         {
             return forwardHome(
-                { success => "The $department_name has been updated successfully" }, 'department/' );
+                { success => "The $department_name has been created successfully" }, 'settings/department_overview/' );
         }
     }
 
-    if ($id && body_parameters->get('delete'))
+    my $base_url = request->base;
+
+    $department->{type}        = $department_name;
+    $department->{description} = "In this window you can add a $department_name to assign to users.";
+    $department->{back_url}    = "${base_url}settings/department_overview/";
+    $department->{field_label} = ucfirst($department_name);
+
+    template 'layouts/page_save_name_only' => {
+        page            => 'department',
+        body_class      => 'page',
+        container_class => 'container-fluid',
+        main_class      => 'main col-lg-10',
+        item            => $department
+    };
+};
+
+any ['get', 'post'] => '/settings/department_edit/:id' => require_any_role [qw/useradmin superadmin/] => sub {
+    my $id = route_parameters->get('id');
+    my $department = schema->resultset('Department')->find($id);
+    my $department_name = lcfirst(var('site')->department_name);
+
+    if (body_parameters->get('submit')) {
+        $department->name(body_parameters->get('name'));
+        if (process(sub {$department->insert_or_update})) {
+            return forwardHome(
+                { success => "The $department_name has been updated successfully" }, 'settings/department_overview/');
+        }
+    }
+
+    if (param('delete')) {
+        if (process(sub {$department->delete_department})) {
+            return forwardHome(
+                { success => "The $department_name has been deleted successfully" }, 'settings/department_overview/');
+        }
+    }
+
+    my $base_url = request->base;
+
+    $department->{type} = $department_name;
+    $department->{description} = "In this window you can edit a ${department_name}. Changes will impact all users currently assigned if you delete or edit a value.";
+    $department->{back_url} = "${base_url}settings/department_overview/";
+    $department->{field_label} = ucfirst($department_name);
+
+    template 'layouts/page_save_name_only' => {
+        page            => 'department',
+        body_class      => 'page',
+        container_class => 'container-fluid',
+        main_class      => 'main col-lg-10',
+        item            => $department
+    };
+};
+
+any ['get', 'post'] => '/settings/department_add/' => require_any_role [qw/useradmin superadmin/] => sub {
+    my $department      = schema->resultset('Department')->new({});
+    my $department_name = lcfirst(var('site')->department_name);
+
+    if (body_parameters->get('submit'))
+    {
+        $department->name(body_parameters->get('name'));
+        if (process( sub { $department->insert_or_update } ))
+        {
+            return forwardHome(
+                { success => "The $department_name has been created successfully" }, 'settings/department_overview/' );
+        }
+    }
+
+    my $base_url = request->base;
+
+    $department->{type}        = $department_name;
+    $department->{description} = "In this window you can add a $department_name to assign to users.";
+    $department->{back_url}    = "${base_url}settings/department_overview/";
+    $department->{field_label} = ucfirst($department_name);
+
+    template 'layouts/page_save_name_only' => {
+        page            => 'department',
+        body_class      => 'page',
+        container_class => 'container-fluid',
+        main_class      => 'main col-lg-10',
+        item            => $department
+    };
+};
+
+any ['get', 'post'] => '/settings/department_edit/:id' => require_any_role [qw/useradmin superadmin/] => sub {
+    my $id              = route_parameters->get('id');
+    my $department      = schema->resultset('Department')->find($id);
+    my $department_name = lcfirst(var('site')->department_name);
+
+    if (body_parameters->get('submit'))
+    {
+        $department->name(body_parameters->get('name'));
+        if (process( sub { $department->insert_or_update } ))
+        {
+            return forwardHome(
+                { success => "The $department_name has been updated successfully" }, 'settings/department_overview/' );
+        }
+    }
+
+    if (param('delete'))
     {
         if (process( sub { $department->delete_department } ))
         {
             return forwardHome(
-                { success => "The $department_name has been deleted successfully" }, 'department/' );
+                { success => "The $department_name has been deleted successfully" }, 'settings/department_overview/' );
         }
     }
 
-    template 'department' => {
-        department  => defined $id && $department,
-        departments => [schema->resultset('Department')->ordered],
-        page        => 'department',
-    };
+    my $base_url = request->base;
 
+    $department->{type}        = $department_name;
+    $department->{description} = "In this window you can edit a ${department_name}. Changes will impact all users currently assigned if you delete or edit a value.";
+    $department->{back_url}    = "${base_url}settings/department_overview/";
+    $department->{field_label} = ucfirst($department_name);
+
+    template 'layouts/page_save_name_only' => {
+        page            => 'department',
+        body_class      => 'page',
+        container_class => 'container-fluid',
+        main_class      => 'main col-lg-10',
+        item            => $department
+    };
 };
 
 any ['get', 'post'] => '/team/?:id?' => require_any_role [qw/useradmin superadmin/] => sub {
