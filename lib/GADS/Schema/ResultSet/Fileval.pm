7809b01a2bdd0fc7dd9c2dc861440a938377bfdc
Andy Beverley <a.beverley@ctrlo.com>
2022-02-19 12:14:21 +0000

    Add missing resultset file

---
+++ lib/GADS/Schema/ResultSet/Fileval.pm
@@ -0,0 +1,18 @@
+package GADS::Schema::ResultSet::Fileval;
+
+use strict;
+use warnings;
+
+use parent 'DBIx::Class::ResultSet';
+
+use Log::Report 'linkspace';
+
+sub independent
+{   shift->search_rs({
+        is_independent => 1,
+    },{
+        order_by => 'me.id',
+    });
+}
+
+1;
