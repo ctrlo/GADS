1c4bb470fc969d1e69e6acb22f302e1acc98cab3
danny cobussen <danny@digitpaint.nl>
2022-12-29 11:30:23 +0100

    Remove rollup config and watch scripts

--- src/polyfills/array.foreach.js
+++
@@ -1,8 +0,0 @@
-if (typeof Array.prototype.forEach != "function") {
-  // eslint-disable-next-line no-extend-native
-  Array.prototype.forEach = function(callback) {
-    for (var i = 0; i < this.length; i++) {
-      callback.apply(this, [this[i], i, this]);
-    }
-  };
-}
