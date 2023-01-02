9698d643c647d514d10e43b642e69de70d6ae6c9
Andy Beverley <andy@andybev.com>
2023-01-02 23:00:24 +0000

    Also allow hyphen in table short name

--- lib/GADS/Layout.pm
+++ lib/GADS/Layout.pm
@@ -607,7 +607,7 @@ sub write
     {
         error __x"Invalid short name for table \"{name}\": short names can only contain alphanumeric characters and the underscore",
             name => $self->name_short
-                unless $self->name_short =~ /^[_a-z0-9]+$/i;
+                unless $self->name_short =~ /^[-_a-z0-9]+$/i;
 
         my $exists = $self->schema->resultset('Instance')->search({
             name_short  => $self->name_short,
