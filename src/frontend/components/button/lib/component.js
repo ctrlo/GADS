ca223fd2a1ffa62eaa3c45159e0618d10755a0a6
Dave Roberts <145559566+droberts-ctrlo@users.noreply.github.com>
2023-10-24 13:19:21 +0100

    Integrate testing into reporting (#8)
    * Jest package changes
    
    * Unit test fixes
    
    Have made unit testing work - "test" folder contains stubs or full implementations for use within testing (as babel is used and doesn't "like" certain paths because of exclusion rules, causing tests to fail) - will explain if required in face-to-face meeting this week with AB
    
    * Undid accidental merge

--- src/frontend/components/button/lib/component.js
+++ src/frontend/components/button/lib/component.js
@@ -2,7 +2,6 @@ import { Component } from 'component'
 import { logging } from 'logging'
 import { MoreInfoButton } from './more-info-button'
 import { validateRequiredFields } from 'validation'
-import CreateReportButtonComponent from './create-report-button'
 
 class ButtonComponent extends Component {
   constructor(element)  {
@@ -15,11 +14,8 @@ class ButtonComponent extends Component {
 
   initButton() {
     switch (true) {
-      case this.el.hasClass('btn-js-report'):
-        new CreateReportButtonComponent(this.element);
-        break;
       case this.el.hasClass('btn-js-more-info'):
-        new MoreInfoButton(this.el)
+        const moreInfoButton = new MoreInfoButton(this.el)
         break
       case this.el.hasClass('btn-js-delete'):
         this.initDelete()
