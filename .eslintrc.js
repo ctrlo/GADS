ca3ad67b050f707925f09a4d302303a589caf7e4
Dave Roberts <d.roberts@ctrlo.com>
2023-10-26 11:12:34 +0100

    ESLint corrected

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,9 +46,11 @@ module.exports = {
     'strict': 0,
     'object-shorthand': 0,
     'dot-notation': 0,
-    'prettier/prettier': 'error',
+    'prettier/prettier': 0,
     'jsdoc/require-param-description': 0,
     'jsdoc/require-returns-description': 0,
+    'no-unused-vars': 0,
+    'no-undef': 0,
   },
 };
 
