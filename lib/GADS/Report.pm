ff3df566f51d2f2c26d5d03fe5e64232771dcf65
Dave Roberts <d.roberts@ctrlo.com>
2023-10-27 09:54:11 +0100

    Report class refactoring

--- lib/GADS/Report.pm
+++ lib/GADS/Report.pm
@@ -18,250 +18,127 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =cut
 
-package GADS::Report;
+# package GADS::Report;
 
-use Data::Dumper;
-use Log::Report 'linkspace';
-use Safe;
-use Moo;
-use MooX::Types::MooseLike::Base qw(:all);
-use namespace::clean;
+# use Data::Dumper;
+# use Log::Report 'linkspace';
+# use Safe;
+# use Moo;
+# use MooX::Types::MooseLike::Base qw(:all);
+# use namespace::clean;
 
-use Dancer2;
-use Dancer2::Plugin::DBIC;
+# use Dancer2;
+# use Dancer2::Plugin::DBIC;
 
-has id => (
-    is       => 'ro',
-    required => 1,
-);
+# has id => (
+#     is       => 'ro',
+#     required => 1,
+# );
 
-has name => (
-    is       => 'rw',
-    required => 0,
-);
+# has name => (
+#     is       => 'rw',
+#     required => 0,
+# );
 
-has description => (
-    is       => 'rw',
-    required => 0,
-);
+# has description => (
+#     is       => 'rw',
+#     required => 0,
+# );
 
-has user_id => (
-    is       => 'rw',
-    required => 0,
-);
+# has user_id => (
+#     is       => 'rw',
+#     required => 0,
+# );
 
-has user => (
-    is      => 'rwp',
-    lazy    => 1,
-    builder => sub {
-        my $self = shift;
-        my $user = schema->resultset('User')->find( $self->user_id );
-        return $user;
-    }
-);
+# has user => (
+#     is      => 'rwp',
+#     lazy    => 1,
+#     builder => sub {
+#         my $self = shift;
+#         my $user = schema->resultset('User')->find( $self->user_id );
+#         return $user;
+#     }
+# );
 
-has createdby => (
-    is       => 'rw',
-    required => 0,
-);
+# has createdby => (
+#     is       => 'rw',
+#     required => 0,
+# );
 
-has createdby_user => (
-    is      => 'rwp',
-    lazy    => 1,
-    builder => sub {
-        my $self = shift;
-        if ( $self->createdby ) {
-            my $user = schema->resultset('User')->find( $self->createdby );
-            return $user;
-        }
-        return $self->user;
-    },
-);
+# has createdby_user => (
+#     is      => 'rwp',
+#     lazy    => 1,
+#     builder => sub {
+#         my $self = shift;
+#         if ( $self->createdby ) {
+#             my $user = schema->resultset('User')->find( $self->createdby );
+#             return $user;
+#         }
+#         return $self->user;
+#     },
+# );
 
-has created => (
-    is       => 'rw',
-    required => 0,
-);
+# has created => (
+#     is       => 'rw',
+#     required => 0,
+# );
 
-has instance_id => (
-    is       => 'rw',
-    required => 0,
-);
+# has instance_id => (
+#     is       => 'rw',
+#     required => 0,
+# );
 
-has instance => (
-    is      => 'rwp',
-    lazy    => 1,
-    builder => sub {
-        my $self = shift;
-        my $instance =
-          schema->resultset('Instance')->find( $self->instance_id );
-        return $instance;
-    },
-);
+# has instance => (
+#     is      => 'rwp',
+#     lazy    => 1,
+#     builder => sub {
+#         my $self = shift;
+#         my $instance =
+#           schema->resultset('Instance')->find( $self->instance_id );
+#         return $instance;
+#     },
+# );
 
-has layout_ids => (
-    is       => 'rw',
-    required => 0,
-);
+# has layout_ids => (
+#     is       => 'rw',
+#     required => 0,
+# );
 
-has layouts => (
-    is      => 'rwp',
-    lazy    => 1,
-    builder => sub {
-        my $self = shift;
-        my @layouts;
-        if ( $self->layout_ids ) {
-            @layouts =
-              map { schema->resultset('Layout')->search( { id => $_ } ) }
-              @{ $self->layout_ids };
-        }
-        return \@layouts;
-    },
-);
+# has layouts => (
+#     is      => 'rwp',
+#     lazy    => 1,
+#     builder => sub {
+#         my $self = shift;
+#         my @layouts;
+#         if ( $self->layout_ids ) {
+#             @layouts =
+#               map { schema->resultset('Layout')->search( { id => $_ } ) }
+#               @{ $self->layout_ids };
+#         }
+#         return \@layouts;
+#     },
+# );
 
-has report_id => (
-    is       => 'rw',
-    required => 0,
-);
+# has report_id => (
+#     is       => 'rw',
+#     required => 0,
+# );
 
-has data => (
-    is      => 'rwp',
-    lazy    => 1,
-    builder => sub {
-        my $self = shift;
+# has data => (
+#     is      => 'rwp',
+#     lazy    => 1,
+#     builder => sub {
+#         my $self = shift;
 
-        my $result = [];
+#         my $result = [];
 
-        foreach my $layout ( @{ $self->layouts } ) {
-            my $data = $self->_load_record_data($layout);
-            push( @{$result}, $data );
-        }
+#         foreach my $layout ( @{ $self->layouts } ) {
+#             my $data = $self->_load_record_data($layout);
+#             push( @{$result}, $data );
+#         }
 
-        return $result;
-    },
-);
+#         return $result;
+#     },
+# );
 
-sub load_all_reports {
-    my $instance_id = shift;
-
-    die "Invalid layout provided"
-      unless $instance_id && $instance_id =~ /^\d+$/;
-
-    my $reports = schema->resultset('Report')->search(
-        {
-            instance_id => $instance_id,
-        }
-    );
-
-    my @result;
-
-    while ( my $next = $reports->next ) {
-        my $report = GADS::Report->new(
-            id          => $next->id,
-            name        => $next->name,
-            description => $next->description,
-            user_id     => $next->user_id,
-            createdby   => $next->createdby->id,
-            instance_id => $next->instance_id,
-            created => DateTime::Format::Pg->parse_datetime( $next->created ),
-        );
-        my $layouts = schema->resultset('ReportLayout')->search(
-            {
-                report_id => $next->id,
-            }
-        );
-        while ( my $layout = $layouts->next ) {
-            $report->add_layout( $layout->layout_id );
-        }
-        push( @result, $report );
-    }
-
-    return \@result;
-}
-
-sub add_layout {
-    my $self      = shift;
-    my $layout_id = shift;
-
-    die "You aren't doing it right" unless ref($self) eq __PACKAGE__;
-    die "No layout id provided"     unless $layout_id;
-
-    if ( !$self->layout_ids ) {
-        $self->layout_ids( [] );
-    }
-
-    push @{ $self->layout_ids }, $layout_id;
-}
-
-sub load {
-    my $self = shift;
-
-    #assume I'm going to make a mistake some time!
-    die "You aren't doing it right" unless ref($self) eq __PACKAGE__;
-    die "No report id provided"     unless $self->id && $self->id =~ /^\d+$/;
-
-    my $report = schema->resultset('Report')->find( $self->id );
-
-    die "No report found" unless $report;
-
-    $self->name( $report->name );
-    $self->description( $report->description );
-    $self->user_id( $report->user_id );
-    $self->createdby( $report->createdby->id );
-    $self->instance_id( $report->instance_id );
-    $self->created( DateTime::Format::Pg->parse_datetime( $report->created ) );
-
-    my $layouts = schema->resultset('ReportLayout')->search(
-        {
-            report_id => $self->id,
-        }
-    );
-    while ( my $layout = $layouts->next ) {
-        $self->add_layout( $layout->layout_id );
-    }
-}
-
-sub _load_record_data {
-    my $self      = shift;
-    my $layout    = shift;
-    my $record_id = $self->report_id;
-    my $user      = $self->user;
-
-    #TODO: This is dirty - make this work properly
-    $record_id = 1 if !$record_id;
-
-    my $gads_layout = GADS::Layout->new(
-        schema      => schema,
-        user        => $user,
-        instance_id => $self->instance_id,
-    );
-
-    my $record = GADS::Record->new(
-        schema => schema,
-        user   => $user,
-        layout => $gads_layout,
-    );
-    $record->find_current_id($record_id);
-
-    my $column = $self->_find_column( $layout->name, $gads_layout->columns );
-
-    my $datum = $record->get_field_value($column);
-
-    return { 'name' => $layout->name, 'value' => $datum };
-}
-
-sub _find_column {
-    my $self        = shift;
-    my $column_name = shift;
-    my $columns     = shift;
-
-    foreach my $col ( @{$columns} ) {
-        if ( $col->name eq $column_name ) {
-            return $col;
-        }
-    }
-
-    return undef;
-}
-
-1;
+# 1;
--- lib/GADS/Schema/Result/Report.pm
+++ lib/GADS/Schema/Result/Report.pm
@@ -1,4 +1,5 @@
 use utf8;
+
 package GADS::Schema::Result::Report;
 
 =head1 NAME
@@ -7,10 +8,10 @@ GADS::Schema::Result::Report
 
 =cut
 
-use strict;
-use warnings;
+use Moo;
 
-use base 'DBIx::Class::Core';
+extends 'DBIx::Class::Core';
+sub BUILDARGS { $_[2] || {} }
 
 =head1 COMPONENTS LOADED
 
@@ -22,7 +23,7 @@ use base 'DBIx::Class::Core';
 
 =cut
 
-__PACKAGE__->load_components("InflateColumn::DateTime");
+__PACKAGE__->load_components( "InflateColumn::DateTime", "+GADS::DBIC" );
 
 =head1 TABLE: C<report>
 
@@ -88,7 +89,11 @@ __PACKAGE__->add_columns(
     "createdby",
     { data_type => "bigint", is_foreign_key => 1, is_nullable => 1 },
     "created",
-    { data_type => "datetime", datetime_undef_if_invalid => 1, is_nullable => 1 },
+    {
+        data_type                 => "datetime",
+        datetime_undef_if_invalid => 1,
+        is_nullable               => 1
+    },
     "instance_id",
     { data_type => "bigint", is_foreign_key => 1, is_nullable => 1 },
 );
@@ -121,9 +126,9 @@ __PACKAGE__->belongs_to(
     { id => "user_id" },
     {
         is_deferrable => 1,
-        join_type => "LEFT",
-        on_delete => "NO ACTION",
-        on_update => "NO ACTION"
+        join_type     => "LEFT",
+        on_delete     => "NO ACTION",
+        on_update     => "NO ACTION"
     },
 );
 
@@ -133,9 +138,9 @@ __PACKAGE__->belongs_to(
     { id => "createdby" },
     {
         is_deferrable => 1,
-        join_type => "LEFT",
-        on_delete => "NO ACTION",
-        on_update => "NO ACTION"
+        join_type     => "LEFT",
+        on_delete     => "NO ACTION",
+        on_update     => "NO ACTION"
     },
 );
 
@@ -153,9 +158,9 @@ __PACKAGE__->belongs_to(
     { id => "instance_id" },
     {
         is_deferrable => 1,
-        join_type => "LEFT",
-        on_delete => "NO ACTION",
-        on_update => "NO ACTION"
+        join_type     => "LEFT",
+        on_delete     => "NO ACTION",
+        on_update     => "NO ACTION"
     },
 );
 
@@ -171,16 +176,158 @@ __PACKAGE__->has_many(
     "report_layouts",
     "GADS::Schema::Result::ReportLayout",
     { "foreign.report_id" => "self.id" },
-    { cascade_copy => 0, cascade_delete => 0 },
+    { cascade_copy        => 0, cascade_delete => 0 },
 );
 
 #TODO: need to work out how to implement this properly
 sub validate {
-    my ($self, $value, %options) = @_;
+    my ( $self, $value, %options ) = @_;
     return 1 if !$value;
+
+    my $name = $self->name;
+    my $instance_id = $self->instance_id;
+    my $layouts = $self->report_layouts;
 
     return 0 unless $options{fatal};
     return 1;
+}
+
+has _is_new => (
+    is      => 'rwp',
+    default => 1,
+    builder => sub {
+        my $self = shift;
+        return 0 if $self->id;
+        return 1;
+    },
+);
+
+has schema => (
+    is       => 'rw',
+    required => 0,
+);
+
+has record_id => (
+    is       => 'rw',
+    required => 0,
+);
+
+has data => (
+    is      => 'rwp',
+    lazy    => 1,
+    builder => sub {
+        my $self = shift;
+
+        my $result = [];
+
+        my $layouts = $self->report_layouts;
+
+        while ( my $layout = $layouts->next ) {
+            my $data = $self->_load_record_data($layout);
+            push( @{$result}, $data );
+        }
+
+        return $result;
+    },
+);
+
+sub load_all_reports {
+    my $instance_id = shift;
+    my $schema      = shift;
+
+    die "Invalid layout provided"
+      unless $instance_id && $instance_id =~ /^\d+$/;
+
+    my $items = $schema->resultset('Report')->search(
+        {
+            instance_id => $instance_id,
+        }
+    );
+
+    my $result = [];
+
+    while ( my $next = $items->next ) {
+        $next->schema($schema) if !$next->schema;
+        push( @{$result}, $next );
+    }
+
+    return $result;
+}
+
+# sub add_layout {
+#     my $self      = shift;
+#     my $layout_id = shift;
+
+#     die "You aren't doing it right" unless ref($self) eq __PACKAGE__;
+#     die "No layout id provided"     unless $layout_id;
+
+#     if ( !$self->layout_ids ) {
+#         $self->layout_ids( [] );
+#     }
+
+#     push @{ $self->layout_ids }, $layout_id;
+# }
+
+sub _load_record_data {
+    my $self      = shift;
+    my $layout    = shift;
+    my $record_id = $self->record_id;
+    my $user      = $self->user;
+
+    die "No report id given" if !$record_id;
+
+    my $gads_layout = GADS::Layout->new(
+        schema      => $self->schema,
+        user        => $user,
+        instance_id => $self->instance_id,
+    );
+
+    my $record = GADS::Record->new(
+        schema => $self->schema,
+        user   => $user,
+        layout => $gads_layout,
+    );
+    $record->find_current_id($record_id);
+
+    my $column = $self->_find_column( $layout->layout->name, $gads_layout->columns );
+
+    my $datum = $record->get_field_value($column);
+
+    return { 'name' => $layout->layout->name, 'value' => $datum };
+}
+
+sub _find_column {
+    my $self        = shift;
+    my $column_name = shift;
+    my $columns     = shift;
+
+#TODO: I know I could probably do this with a grep, but for some reason, I can't get said grep to work
+#grep { $_->name eq $column_name } @{$columns};
+    foreach my $col ( @{$columns} ) {
+        if ( $col->name eq $column_name ) {
+            return $col;
+        }
+    }
+
+    return undef;
+}
+
+sub load {
+    my $id        = shift;
+    my $record_id = shift;
+    my $schema    = shift;
+
+    die "Invalid report id provided"
+      unless $id && $id =~ /^\d+$/;
+
+    my $result = $schema->resultset('Report')->find($id)
+      or die "No report found for id $id";
+    $result->schema($schema) if !$result->schema;
+    $result->record_id($record_id)
+      if $record_id
+      && !$result->record_id;
+
+    return $result;
 }
 
 1;
