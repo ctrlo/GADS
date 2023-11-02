932ba93cb8375198b5105107411fdbb71cbaddcf
Dave Roberts <d.roberts@ctrlo.com>
2023-11-02 13:19:40 +0000

    List condition filters
    Listing condition filters complete

--- src/frontend/components/form-group/display-conditions/lib/component.js
+++ src/frontend/components/form-group/display-conditions/lib/component.js
@@ -1,34 +1,111 @@
-import { Component } from 'component'
-import queryBuilder from '@lol768/jquery-querybuilder-no-eval/dist/js/query-builder.standalone.min'
+import { Component } from "component";
+import queryBuilder from "@lol768/jquery-querybuilder-no-eval/dist/js/query-builder.standalone.min";
 
 class DisplayConditionsComponent extends Component {
-  constructor(element)  {
-    super(element)
-    this.el = $(this.element)
-    this.initDisplayConditions()
+  constructor(element) {
+    super(element);
+    this.el = $(this.element);
+    this.initDisplayConditions();
   }
 
   initDisplayConditions() {
-    const builderData = this.el.data()
-    const filters = JSON.parse(Buffer.from(builderData.filters, 'base64'))
-    if (!filters.length) return
+    this.buildConditions(this.el);
+  }
 
-    this.el.queryBuilder({
-      filters: filters,
+  setupSelects(el) {
+    const selects = el.find("select");
+    const self = this;
+    selects.hide();
+    selects.each(function () {
+      const div = document.createElement("div");
+      div.className = "input-container";
+      const element = document.createElement("input");
+      element.type = "text";
+      element.className = "form-control";
+      $(element).on("keyup", (ev) => self.textChange(ev));
+      div.prepend(element);
+      const itemsDiv = document.createElement("div");
+      itemsDiv.className = "items";
+      $(this)
+        .find("option")
+        .each(function () {
+          if (this.text === "------") return;
+          const item = document.createElement("div");
+          item.className = "item";
+          item.dataset.value = this.value;
+          item.innerHTML = this.text;
+          $(item).on("click", (ev) => self.itemClick(ev));
+          itemsDiv.append(item);
+        });
+      div.append(itemsDiv);
+      this.closest(".rule-filter-container").append(div);
+    });
+  }
+
+  itemClick(ev) {
+    const $container = $(ev.target).closest(".rule-filter-container");
+    const $input = $container.find("[type='text']");
+    $input.val(ev.target.innerHTML);
+    const $select = $container.find("select");
+    $select.val(ev.target.dataset.value);
+    $select.trigger("change");
+  }
+
+  textChange(ev) {
+    const $container = $(ev.target).closest(".rule-filter-container");
+    const $select = $container.find("select");
+    const $items = $container.find(".items");
+    const self = this;
+    $items.children().remove();
+    $select.find("option").each(function () {
+      if (this.text === "------") return;
+      if (this.text === "") {
+        const item = document.createElement("div");
+        item.className = "item";
+        item.dataset.value = this.value;
+        item.innerHTML = this.text;
+        $(item).on("click", (ev) => self.itemClick(ev));
+        $items.append(item);
+      } else if (
+        this.text.toLowerCase().includes(ev.target.value.toLowerCase())
+      ) {
+        const item = document.createElement("div");
+        item.className = "item";
+        item.dataset.value = this.value;
+        item.innerHTML = this.text;
+        $(item).on("click", (ev) => self.itemClick(ev));
+        $items.append(item);
+      }
+    });
+  }
+
+  buildConditions(el) {
+    const self = this;
+
+    el.on("afterCreateRuleFilters.queryBuilder", (event, filters) => {
+      self.setupSelects($(event.target));
+    });
+
+    const builderData = el.data();
+    const filters = JSON.parse(Buffer.from(builderData.filters, "base64"));
+    if (!filters.length) return;
+
+    const builder = el.queryBuilder({
+      filters,
       allow_groups: 0,
       operators: [
-        { type: 'equal', accept_values: true, apply_to: ['string'] },
-        { type: 'contains', accept_values: true, apply_to: ['string'] },
-        { type: 'not_equal', accept_values: true, apply_to: ['string'] },
-        { type: 'not_contains', accept_values: true, apply_to: ['string'] }
-      ]
-    })
+        { type: "equal", accept_values: true, apply_to: ["string"] },
+        { type: "contains", accept_values: true, apply_to: ["string"] },
+        { type: "not_equal", accept_values: true, apply_to: ["string"] },
+        { type: "not_contains", accept_values: true, apply_to: ["string"] },
+      ],
+    });
 
     if (builderData.filterBase) {
-      const data = Buffer.from(builderData.filterBase, 'base64')
-      this.el.queryBuilder('setRules', JSON.parse(data))
+      const data = Buffer.from(builderData.filterBase, "base64");
+      el.queryBuilder("setRules", JSON.parse(data));
     }
   }
 }
 
-export default DisplayConditionsComponent
+export default DisplayConditionsComponent;
---
+++ src/frontend/components/form-group/display-conditions/lib/select.test.js
@@ -0,0 +1,83 @@
+import DisplayConditionsComponent from "./component";
+
+global.$ = require("jquery");
+
+describe("tests for component replacement", () => {
+  const getDiv = () => {
+    const div = document.createElement("div");
+    div.classList.add("rule-filter-container");
+    div.id = "displayConditionsBuilder";
+    $(div).data(
+      "filters",
+      "W3siaWQiOiI4IiwidHlwZSI6InN0cmluZyIsImxhYmVsIjoiVGVzdCJ9LHsidHlwZSI6InN0cmluZyIsImxhYmVsIjoiVGVzdCBPdGhlciBWYWx1ZSIsImlkIjoiMTEifSx7ImlkIjoiMTIiLCJ0eXBlIjoic3RyaW5nIiwibGFiZWwiOiJUZXN0IFJBRyJ9LHsiaWQiOiIxMyIsInR5cGUiOiJzdHJpbmciLCJsYWJlbCI6IlBlcnNvbiBUZXN0In1d"
+    );
+    return div;
+  };
+
+  it("should replace selects with inputs", () => {
+    const div = getDiv();
+    new DisplayConditionsComponent(div);
+    expect($(div).find('input[type="text"]').length).toBe(1);
+    expect($(div).find("select").length).toBe(1);
+  });
+
+  it("should have list of options below inputs", () => {
+    const div = getDiv();
+    new DisplayConditionsComponent(div);
+    expect($(div).find(".item").length).toBe(4);
+  });
+
+  it("should change the select value on click of the item", () => {
+    const div = getDiv();
+    new DisplayConditionsComponent(div);
+    const item = $(div).find(".item")[0];
+    expect(item.dataset.value).toBe("8");
+    $(item).trigger("click");
+    expect($(div).find("select").val()).toBe("8");
+  });
+
+  it("should provide a list of options below according to text entered", () => {
+    const div = getDiv();
+    new DisplayConditionsComponent(div);
+    const input = $(div).find('input[type="text"]');
+    input.val("Test");
+    input.trigger("keyup");
+    expect($(div).find(".item").length).toBe(4);
+    input.val("er");
+    input.trigger("keyup");
+    expect($(div).find(".item").length).toBe(2);
+  });
+
+  it("should provide a list of all items below if no text is in the textbox", () => {
+    const div = getDiv();
+    new DisplayConditionsComponent(div);
+    const input = $(div).find('input[type="text"]');
+    input.val("er");
+    input.trigger("keyup");
+    expect($(div).find(".item").length).toBe(2);
+    input.val("");
+    input.trigger("keyup");
+    expect($(div).find(".item").length).toBe(4);
+  });
+
+  it("should change the textarea value on click", () => {
+    const div = getDiv();
+    new DisplayConditionsComponent(div);
+    const input = $(div).find('input[type="text"]');
+    input.val("Test");
+    input.trigger("keyup");
+    const item = $(div).find(".item")[0];
+    const firstOption = $(div).find("option")[1]; //Actual first option is "------" which is ignored!
+    $(item).trigger("click");
+    expect($(div).find('input[type="text"]').val()).toBe(firstOption.text);
+  });
+
+  it("should have all values from the select options used below the textbox", () => {
+    const div = getDiv();
+    new DisplayConditionsComponent(div);
+    const select = $(div).find("select");
+    const options = $(select).find("option");
+    const items = $(div).find(".item");
+    expect(options.length).toBe(items.length + 1);
+  });
+});
