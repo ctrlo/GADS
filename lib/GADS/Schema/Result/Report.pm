be5aebd53600d3f10263f754ac7e7edd528fd7b1
Dave Roberts <d.roberts@ctrlo.com>
2023-11-14 14:19:59 +0000

    Stopped being a donut!

--- lib/GADS/Schema/Result/Report.pm
+++ lib/GADS/Schema/Result/Report.pm
@@ -223,7 +223,7 @@ has data => (
         my $allowed_cols = [map {$_->name} $gads_layout->all(user_can_read => 1)];
 
          while ( my $layout = $layouts->next ) {
-            next unless $self->_is_column_allowed($self, $layout->layout->name, $allowed_cols);
+            next unless $self->_is_column_allowed($layout->layout->name, $allowed_cols);
             my $column = $gads_layout->column( $layout->layout_id );
             my $datum  = $record->get_field_value($column);
             my $data   = { 'name' => $layout->layout->name, 'value' => $datum || '' };
