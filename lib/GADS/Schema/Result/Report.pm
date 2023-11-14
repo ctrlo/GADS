71e318ea23931e4286290d5a2af46d73b1fc5d3f
Dave Roberts <d.roberts@ctrlo.com>
2023-11-14 14:07:31 +0000

    SECURITY FIX - access to disallowed columns
    Users could access all data from a record in a report regardless of access - this is now fixed

--- lib/GADS/Schema/Result/Report.pm
+++ lib/GADS/Schema/Result/Report.pm
@@ -220,17 +220,31 @@ has data => (
 
         my $record_id = $self->record_id;
         $record->find_current_id($record_id);
+        my $allowed_cols = [map {$_->name} $gads_layout->all(user_can_read => 1)];
 
-        while ( my $layout = $layouts->next ) {
+         while ( my $layout = $layouts->next ) {
+            next unless _is_column_allowed($self, $layout->layout->name, $allowed_cols);
             my $column = $gads_layout->column( $layout->layout_id );
             my $datum  = $record->get_field_value($column);
-            my $data   = { 'name' => $layout->layout->name, 'value' => $datum };
+            my $data   = { 'name' => $layout->layout->name, 'value' => $datum || '' };
             push( @{$result}, $data );
         }
 
         return $result;
     },
 );
+
+sub _is_column_allowed {
+    my ($self, $column, $allowed_cols) = @_;
+
+    foreach my $value (@$allowed_cols) {
+        if($value eq $column) {
+            return 1;
+        }
+    }
+
+    return 0;
+};
 
 =head1 Object functions
 =head2 Update Report
