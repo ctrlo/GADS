a1ca10590b52b1555aee2ceb41c0514101322956
Dave Roberts <d.roberts@ctrlo.com>
2023-11-15 11:53:54 +0000

    Updated test for reporting due to error found

--- src/frontend/components/button/lib/create-report-button.test.js
+++ src/frontend/components/button/lib/create-report-button.test.js
@@ -9,7 +9,7 @@ describe('create-report-button', () => {
       <form id="myform">
         <input class="form-control " id="report_name" name="report_name" value="Something" data-restore-value="" required="" aria-required="true">
         <input class="form-control " id="report_description" name="report_description" placeholder="New Report Description" value="" data-restore-value="">
-        <fieldset class="fieldset checkbox-fieldset--required" id="report_fields">
+        <fieldset class="fieldset fieldset--required checkbox-fieldset--required" id="report_fields">
           <input id="1" type="checkbox">
           <input id="8" type="checkbox">
           <input id="2" type="checkbox">
@@ -39,7 +39,7 @@ describe('create-report-button', () => {
           <input required="" aria-required="true">
         </div>
         <input>
-        <fieldset class="fieldset checkbox-fieldset--required" id="report_fields">
+        <fieldset class="fieldset fieldset--required checkbox-fieldset--required" id="report_fields">
           <input id="1" type="checkbox">
           <input id="8" type="checkbox" checked="checked">
           <input id="2" type="checkbox">
@@ -69,7 +69,7 @@ describe('create-report-button', () => {
           <input required="" aria-required="true" value = "boop">
         </div>
         <input>
-        <fieldset class="fieldset checkbox-fieldset--required" id="report_fields">
+        <fieldset class="fieldset fieldset--required checkbox-fieldset--required" id="report_fields">
           <input id="1" type="checkbox">
           <input id="8" type="checkbox" checked="checked">
           <input id="2" type="checkbox">
@@ -100,7 +100,7 @@ describe('create-report-button', () => {
   it('Validates checkboxes correctly', () => {
     document.body.innerHTML = `
       <form>
-        <fieldset class="fieldset checkbox-fieldset--required">
+        <fieldset class="fieldset fieldset--required checkbox-fieldset--required">
           <div class="fieldset__legend">
             <legend id="-label">
               Columns to show in report:
