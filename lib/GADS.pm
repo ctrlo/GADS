2cfe45d75d03ae6b3f9b211b03d94729472795a5
Dave Roberts <d.roberts@ctrlo.com>
2023-11-20 12:35:39 +0000

    Test fixes
    Fixed error in view causing rendering to fail, and fixed dept. and team display

--- lib/GADS.pm
+++ lib/GADS.pm
@@ -1606,14 +1606,18 @@ any ['get', 'post'] => '/user/:id' => require_any_role [qw/useradmin superadmin/
 
     my $orgs = [map { +{ label_html => $_->name, value => $_->id } } @{$userso->organisations}];
 
+    my $depts = [map {+{label_html=>$_->name, value=>$_->id}} @{$userso->departments}];
+
+    my $teams = [map {+{label_html => $_->name, value => $_->id}} @{$userso->teams}];
+
     my $output = template 'user/user_edit' => {
         edituser => $editUser,
         groups   => GADS::Groups->new(schema => schema)->all,
         values   => {
             title         => $titles,
             organisation  => $orgs,
-            department_id => $userso->departments,
-            team_id       => $userso->teams,
+            department_id => $depts,
+            team_id       => $teams,
         },
         permissions => $userso->permissions,
         page        => 'user',
