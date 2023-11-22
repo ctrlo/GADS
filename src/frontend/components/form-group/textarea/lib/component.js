2abf3cf041541931353da3a4ee03c0eec5e21e04
Oliver-ctrlo <119069310+Oliver-ctrlo@users.noreply.github.com>
2023-11-22 14:11:17 +0000

    Added auto height functionality for textareas
    Enables auto-resize for text areas with class 'auto-adjust'
    adjustTextareaHeight() calculates the total height of the text-area content (by putting it in a hidden div.)
    
    Then calculates the height of one line (in REM).
    
    Then if the content is more than one line, it adds the calculated line height to the total height. If not then just default to total height.

--- src/frontend/components/form-group/textarea/lib/component.js
+++ src/frontend/components/form-group/textarea/lib/component.js
@@ -9,7 +9,50 @@ class TextareaComponent extends Component {
       if (this.el.hasClass("textarea--required")) {
         initValidationOnField(this.el)
       }
+      
+      // Check if there is a textarea with the class 'auto-adjust'
+      const $autoAdjustTextarea = this.$el.find('textarea.auto-adjust');
+      if ($autoAdjustTextarea.length) {
+        this.adjustTextareaHeight();
+
+        $autoAdjustTextarea.on('change', () => {
+            this.adjustTextareaHeight();
+        });
+      }
+    } 
+    adjustTextareaHeight() {
+        const $textarea = this.$el.find('textarea.auto-adjust');
+
+        // Create a hidden div with just the text content
+        const $hiddenDiv = $('<div></div>').text($textarea.val()).css({
+            visibility: 'hidden',
+            position: 'absolute',
+            whiteSpace: 'pre-wrap',
+            padding: $textarea.css('padding'),
+            border: $textarea.css('border'),
+        });
+
+        // Add div to body
+        $('body').append($hiddenDiv);
+
+        // Calc the height + padding + border
+        const contentHeight = $hiddenDiv.outerHeight() / parseFloat(getComputedStyle($hiddenDiv[0]).fontSize);
+
+        // Remove hidden div
+        $hiddenDiv.remove();
+
+        const lineHeight = parseFloat($textarea.css('line-height')) / parseFloat($textarea.css('font-size'));
+        const minHeight = 2.5; // min height in REM
+        const maxHeight = 18;  // max height in REM
+
+        // Calculate the adjusted height
+        let adjustedHeight = (contentHeight < minHeight) ? contentHeight : contentHeight + lineHeight;
+
+        // Apply the maximum height constraint
+        adjustedHeight = Math.min(adjustedHeight, maxHeight);
+
+        // Set textarea height
+        $textarea.css('height', `${adjustedHeight}rem`);
     }
 }
-
 export default TextareaComponent
