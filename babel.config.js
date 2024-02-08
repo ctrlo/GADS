8a7a7f826d31551278df20e96bf5fb76e4b28e73
Dave Roberts <d.roberts@ctrlo.com>
2024-02-08 13:36:31 +0000

    Added new babel config file and added comment as clarification for exclude

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "presets": [
     [
       "@babel/preset-env",
@@ -17,6 +17,7 @@
     "@babel/preset-typescript",
     "@babel/preset-react"
   ],
+  // ignore the plotly files as babel will mash them and they will not work
   "ignore": [
     "src/frontend/js/lib/plotly/*.js",
   ]
