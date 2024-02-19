b8b6dd65f06b723d3e8dcd433b166a810655b30b
Dave Roberts <d.roberts@ctrlo.com>
2024-02-19 17:03:53 +0000

    Updated files ready for testing.

--- src/frontend/js/lib/util/typeahead/lib/Typeahead.ts
+++ src/frontend/js/lib/util/typeahead/lib/Typeahead.ts
@@ -9,9 +9,8 @@ import { TypeaheadSourceOptions } from "./TypeaheadSourceOptions";
  * @param sourceOptions - options for the typeahead data source
  */
 export class Typeahead {
-    private debug = false;
     private timeout = null;
-    ajaxRequest: JQuery.jqXHR<any>;
+    private ajaxRequest: JQuery.jqXHR<any>;
 
     /**
      * Create a new Typeahead class
@@ -35,8 +34,7 @@ export class Typeahead {
         }, {
             name: name,
             source: (query, syncResults, asyncResults) => {
-                if (this.timeout) clearTimeout(this.timeout);
-                this.timeout = setTimeout(() => {
+                this.debounce(() => {
                     const request: JQuery.AjaxSettings<any> = {
                         url: ajaxSource + (appendQuery ?  query : ""),
                         dataType: "json",
@@ -44,19 +42,19 @@ export class Typeahead {
                             this.ajaxRequest && this.ajaxRequest.abort();
                         },
                         success: (data) => {
-                            if (this.debug) console.log("Typeahead data:", data);
+                            if (window.test) console.log("Typeahead data:", data);
                             const mapped = mapper(data);
-                            if (this.debug) console.log("Typeahead mapped data:", mapped);
-                            const filtered = mapped.filter((item: MappedResponse) => {
-                                return item.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-                            });
-                            if (this.debug) console.log("Typeahead filtered data:", filtered);
+                            if (window.test) console.log("Typeahead mapped data:", mapped);
+                            const filtered = this.filterData(mapped, query);
+                            if (window.test) console.log("Typeahead filtered data:", filtered);
                             asyncResults(filtered);
-                        }
+                        },
+                        async: window.test ? false : true,
+                        cache: false
                     };
                     if (this.sourceOptions.data) request.data = this.sourceOptions.data;
                     if (this.sourceOptions.dataBuilder) request.data = this.sourceOptions.dataBuilder();
-                    if (this.debug) console.log("Typeahead request: ", request);
+                    if (window.test) console.log("Typeahead request: ", request);
                     this.ajaxRequest = $.ajax(request);
                 }, 200);
             },
@@ -78,5 +76,53 @@ export class Typeahead {
         this.$input.on('typeahead:select', (ev: any, suggestion: MappedResponse) => {
             this.callback(suggestion);
         });
+
+        if(window.test) {
+            this.$input.on("typeahead:asyncrequest", () => {
+                console.log("Typeahead async request");
+            });
+
+            this.$input.on("typeahead:asyncreceive", () => {
+                console.log("Typeahead async receive");
+            });
+
+            this.$input.on("typeahead:asynccancel", () => {
+                console.log("Typeahead async cancel");
+            });
+        }
+    }
+
+    /**
+     * This function is to filter the data based on the query
+     * @param query The query to use in this filter
+     * @param data The data to filter
+     * @returns The filtered data
+     */
+    filterData(data: MappedResponse[], query?: string): MappedResponse[] {
+        // I know that this was originally above, and _shouldn't_ need to be here, but there is a bug in the filtering somewhere,
+        // and I need to unit test it here to ensure this isn't the problem! - DR 19/02/2024
+        if(!query || !query.length) {
+            if(window.test) console.log("No query, returning data:", data);
+            return data;
+        }
+        if(window.test) console.log("Filtering data with query:", query);
+        return data.filter((item) => {
+            return item.name.toLowerCase().includes(query.toLowerCase());
+        });
+    }
+
+    /**
+     * Debounce function to prevent multiple requests being sent to the server - not required when testing, as we want to trigger immediately!
+     * @param func The function to debounce
+     * @param wait The time to wait before calling the function
+     */
+    debounce(func: Function, wait: number) {
+        if (window.test) {
+            console.log("Test mode - no debounce applied!")
+            return func();
+        }
+        if(this.timeout) clearTimeout(this.timeout);
+        // Weird casting error - having to cast to this in order to stop typing error.
+        this.timeout = setTimeout(<(args:void)=>void>func, wait);
     }
 };
