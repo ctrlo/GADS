7e65404b5292c1fd6bccb86ce9334ea24362cc90
Dave Roberts <d.roberts@ctrlo.com>
2024-03-08 12:16:13 +0000

    Updated commands into a module to stop TypeScript errors

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,36 +35,38 @@
 //     }
 //   }
 // }
+export { }
+
 declare global {
     namespace Cypress {
         interface Chainable {
             /**
              * Get a component by name
              * @param name The name of the element to get
-             * @example cy.getByName('username') - this is the same as `cy.get('[name="username"]')`
+             * @example cy.getByName('username')
              */
             getByName(name: string): Chainable<JQuery<HTMLElement>>;
             /**
              * Get a component by title
              * @param title The title of the element to get
-             * @example cy.getByTitle('username') - this is the same as `cy.get('[title="username"]')`
+             * @example cy.getByTitle('username')
              */
             getByTitle(title: string): Chainable<JQuery<HTMLElement>>;
             /**
              * Login to the application
              * @param email The email to use to login
              * @param password The password to use to login
-             * @example cy.login('username@example.com','password') - this will log in the user with the given email and password
+             * @example cy.login('username@example.com','password')
              */
-            login(email: string, password: string): Chainable<JQuery<Element>>;
+            login(email: string, password: string): void;
             /**
              * Login then navigate to a page
              * @param email The email to use to login
              * @param password The password to use to login
              * @param path The location to navigate to after login
-             * @example cy.loginAndGoTo('username@example.com','password','/home') - this is the same as `cy.login('username@example.com','password').visit('/home')`
+             * @example cy.loginAndGoTo('username@example.com','password','/home')
              */
-            loginAndGoTo(email:string, password:string, path:string): Chainable<JQuery<Element>>;
+            loginAndGoTo(email: string, password: string, path: string): void;
             /**
              * Get the main body of the page
              * @example cy.mainBody() - this is the same as `cy.get('.content-block__main')`
@@ -86,13 +88,13 @@ declare global {
              * @param title The title to create in the system
              * @example cy.createTitle('New Title')
              */
-            createTitle(title:string): Chainable<JQuery<HTMLElement>>;
+            createTitle(title: string): void;
             /**
              * Create an organisation in the system
              * @param title The title of the organisation to create
              * @example cy.createOrganisation('New Organisation')
              */
-            createOrganisation(title:string): Chainable<JQuery<HTMLElement>>;
+            createOrganisation(title: string): void;
         }
     }
 }
@@ -105,44 +107,45 @@ Cypress.Commands.add('getByTitle', (title: string) => {
     return cy.get(`[title=${title}]`);
 });
 
-Cypress.Commands.add("login",(email:string,password:string)=>{
+Cypress.Commands.add("login", (email: string, password: string) => {
     cy.visit('http://localhost:3000');
     cy.get("#username").type(email);
     cy.get("#password").type(password);
     cy.getByName("signin").click();
-    return cy.location("pathname").should("not.include", "/login");
+    cy.location("pathname").should("not.include", "/login");
 });
 
-Cypress.Commands.add('loginAndGoTo', (email:string, password:string, path:string) => {
-    return cy.login(email,password).visit(path);
+Cypress.Commands.add('loginAndGoTo', (email: string, password: string, path: string) => {
+    cy.login(email, password);
+    cy.visit(path);
 });
 
-Cypress.Commands.add('mainBody',()=>{
+Cypress.Commands.add('mainBody', () => {
     return cy.get(".content-block__main");
 });
 
-Cypress.Commands.add('mainHeader',()=>{
+Cypress.Commands.add('mainHeader', () => {
     return cy.get(".content-block__head");
 });
 
-Cypress.Commands.add('getDataTable',()=>{
+Cypress.Commands.add('getDataTable', () => {
     return cy.mainBody().find(".data-table");
 });
 
-Cypress.Commands.add('createTitle',(title:string)=>{
-    if(!location.pathname.match(/title_add/)) {
+Cypress.Commands.add('createTitle', (title: string) => {
+    if (!location.pathname.match(/title_add/)) {
         cy.visit('http://localhost:3000/settings/title_add/');
     }
     cy.mainBody().find("input[name='title']").type(title);
     cy.mainBody().find("button[type='submit']").click();
-    return cy.getDataTable().find("tbody").find("tr").contains(title);
+    cy.getDataTable().find("tbody").find("tr").contains(title);
 });
 
-Cypress.Commands.add('createOrganisation',(title:string)=>{
-    if(!location.pathname.match(/organisation_add/)) {
+Cypress.Commands.add('createOrganisation', (title: string) => {
+    if (!location.pathname.match(/organisation_add/)) {
         cy.visit('http://localhost:3000/settings/organisation_add/');
     }
     cy.mainBody().find("input[name='title']").type(title);
     cy.mainBody().find("button[type='submit']").click();
-    return cy.getDataTable().find("tbody").find("tr").contains(title);
+    cy.getDataTable().find("tbody").find("tr").contains(title);
 });
