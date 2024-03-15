0db2b4ce3ab0c64287495f33e79a7abf92f3e6b0
Dave Roberts <d.roberts@ctrlo.com>
2024-03-15 14:33:00 +0000

    File upload tests complete

--- cypress/e2e/functionality/05-file-upload.cy.ts
+++ cypress/e2e/functionality/05-file-upload.cy.ts
@@ -6,13 +6,75 @@ describe('Files upload', () => {
     });
 
     it("should cancel upload", () => {
-        cy.get("button.btn-default")
+        cy.contains("Upload a file")
+            .click();
+        cy.contains("Cancel").click();
+        cy.get(".modal").should("not.be.visible");
+        cy.getDataTable()
+            .find("tbody")
+            .find("tr")
+            .contains("There is no data available in this table")
+    });
+
+    it("Should error when uploading an invalid file", () => {
+        cy.contains("Upload a file")
+            .click();
+        cy.get(".modal")
+            .find("input[type='file']")
+            .invoke('show')
+            .selectFile("./cypress/fixtures/testfile.html", { force: true });
+        cy.contains("Submit").click();
+        cy.get(".alert-danger").should("be.visible").and("contain", "Files with extension");
+    });
+
+    it("Should upload a file", () => {
+        cy.contains("Upload a file")
+            .click();
+        cy.get(".modal")
+            .find("input[type='file']")
+            .invoke('show')
+            .selectFile("./cypress/fixtures/smiley.png", { force: true });
+        cy.contains("Submit").click();
+        cy.get(".modal").should("not.be.visible");
+        cy.get(".alert-success").should("be.visible").and("contain", "File has been uploaded");
+        cy.getDataTable()
+            .find("tbody")
+            .find("tr")
+            .contains("smiley.png");
+    });
+
+    it("Should cancel deletion of a file", () => {
+        cy.getDataTable()
+            .find("tbody")
+            .find("tr")
+            .find("button")
+            .click();
+        cy.get(".modal")
+            .should("be.visible", { timeout: 10000 })
+            .find("button.btn-cancel")
             .eq(1)
             .click();
-        cy.get(".btn-cancel")
-            .eq(0)
+        cy.get(".modal").should("not.be.visible");
+        cy.getDataTable()
+            .find("tbody")
+            .find("tr")
+            .contains("smiley.png");
+    });
+
+    it("Should delete a file", () => {
+        cy.getDataTable()
+            .find("tbody")
+            .find("tr")
+            .find("button")
+            .click();
+        cy.get(".modal")
+            .should("be.visible", { timeout: 10000 })
+            .find("button.btn-danger")
             .click();
         cy.get(".modal").should("not.be.visible");
-        cy.getDataTable().find("tbody").find("tr").contains("There is no data available in this table")
+        cy.getDataTable()
+            .find("tbody")
+            .find("tr")
+            .contains("There is no data available in this table");
     });
 });
