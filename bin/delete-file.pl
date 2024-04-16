f59e55d1adbeed0f0a73a3561acd4661ca9f731a
Dave Roberts <d.roberts@ctrlo.com>
2024-04-16 10:18:39 +0100

    Fix to work off current_id not record_id

--- bin/delete-file.pl
+++ bin/delete-file.pl
@@ -7,16 +7,20 @@ use feature 'say';
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 
+use Dancer2::Plugin::DBIC;
+
 use GADS::Purge::Purger;
 
-say "Usage: ./delete-file.pl <layout_id> <record_id> [<record_id> ...]" unless @ARGV > 1;
+say "Usage: ./delete-file.pl <layout_id> <current_id> [<current_id> ...]" unless @ARGV > 1;
 exit(1) unless @ARGV > 1;
 
 my $layout_id = shift(@ARGV);
 
 my $total = 0;
 
-foreach my $record_id (@ARGV) {
+foreach my $current_id (@ARGV) {
+    my $current = schema->resultset('Current')->find($current_id);
+    my $record_id = $current->record_id;
     my $purger= GADS::Purge::Purger->new(
         schema => schema,
         record_id => $record_id,
