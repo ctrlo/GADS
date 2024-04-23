3be8e9b9c44847af48e32ce5e684b5f1431fced4
Dave Roberts <d.roberts@ctrlo.com>
2024-04-23 11:59:48 +0100

    Made error client-side
    Made error for client-side and moved button functionality into new file

--- src/frontend/components/button/lib/component.js
+++ src/frontend/components/button/lib/component.js
@@ -29,7 +29,10 @@ class ButtonComponent extends Component {
         this.initDelete()
         break
       case this.el.hasClass('btn-js-submit-field'):
-        this.initSubmitField()
+        import(/* webpackChunkName: "submit-field-button" */ "./submit-field-button.js")
+            .then(({ default: SubmitFieldButtonComponent }) => {
+                new SubmitFieldButtonComponent(this.element);
+            });
         break
       case this.el.hasClass('btn-js-add-all-fields'):
         this.initAddAllFields()
@@ -121,14 +124,6 @@ class ButtonComponent extends Component {
     })
   }
 
-  initSubmitField() {
-    this.el.on('click', (ev) => { this.submitField(ev) })
-  }
-
-  initSubmitDraftRecord() {
-    this.el.on('click', (ev) => { this.submitDraftRecord(ev) })
-  }
-
   initSubmitRecord() {
     this.el.on('click', (ev) => { this.submitRecord(ev) })
   }
@@ -141,71 +136,6 @@ class ButtonComponent extends Component {
 
   initDelete() {
     this.el.on('click', (ev) => { this.dataToModal(ev) })
-  }
-
-  submitField(ev) {
-    const $jstreeContainer = $('#field_type_tree')
-    const $jstreeEl = $('#tree-config .tree-widget-container')
-
-    const $displayConditionsBuilderEl = $('#displayConditionsBuilder')
-    const res = $displayConditionsBuilderEl.length && $displayConditionsBuilderEl.queryBuilder('getRules')
-    const $displayConditionsField = $('#displayConditions')
-
-    const $instanceIDField = $('#refers_to_instance_id')
-    const $filterEl = $instanceIDField.length && $(`[data-builder-id='${$instanceIDField.val()}']`)
-
-    const $permissionTable = $('#default_field_permissions_table');
-
-    let bUpdateTree = false
-    let bUpdateFilter = false
-    let bUpdateDisplayConditions = false
-
-    if (($jstreeContainer.length && $jstreeContainer.is(':visible') && $jstreeEl.length) || (!$jstreeContainer.length && $jstreeEl.length)) {
-      bUpdateTree = true
-    }
-
-    if ($instanceIDField.length && !$instanceIDField.prop('disabled') && $filterEl.length) {
-      bUpdateFilter = true
-    }
-
-    if (res && $displayConditionsField.length) {
-      bUpdateDisplayConditions = true
-    }
-
-    if (bUpdateTree) {
-      const v = $jstreeEl.jstree(true).get_json('#', { flat: false })
-      const mytext = JSON.stringify(v)
-      const data = $jstreeEl.data()
-
-      $.ajax({
-        async: false,
-        type: 'POST',
-        url: this.getURL(data),
-        data: { data: mytext, csrf_token: data.csrfToken }
-      }).done(() => {
-        // eslint-disable-next-line no-alert
-        alert('Tree has been updated')
-      })
-    }
-
-    if (bUpdateFilter) {
-      window.UpdateFilter($filterEl, ev)
-    }
-
-    if (bUpdateDisplayConditions) {
-      $displayConditionsField.val(JSON.stringify(res, null, 2))
-    }
-
-    /* By default, if the permissions datatable is paginated, then the
-     * permission checkboxes on other pages will not be submitted and will
-     * therefore be cleared. This code gets all the inputs in the datatable
-     * and appends them to the form manually */
-    var $inputs = $permissionTable.DataTable().$('input,select,textarea')
-    $inputs.hide() // Stop them appearing to the user in a strange format
-    let $form = $(ev.currentTarget).closest('form')
-    $permissionTable.remove()
-    $form.append($inputs)
-
   }
 
   submitDraftRecord(ev) {
