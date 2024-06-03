1b0d58e164b9f7b2dba8a0a13694ad1e7504b5c3
Dave Roberts <d.roberts@ctrlo.com>
2024-06-03 16:54:12 +0100

    Made script more specific
    Made functionality specific to only files - this is to ensure no errors (ticket 1615)

--- bin/delete-file.pl
+++ bin/delete-file.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-use feature ':5.37';
+use feature 'say';
 
 use FindBin;
 use lib "$FindBin::Bin/../lib";
@@ -12,70 +12,33 @@ use Encode 'encode';
 use Dancer2;
 use Dancer2::Plugin::DBIC;
 
-sub purge {
-    my $value = shift;
-    if(ref $value eq 'GADS::Schema::Result::Calcval') {
-        purge_calc $value;
-    }elsif(ref $value eq 'GADS::Schema::Result::Date') {
-        purge_value ($value, 'Date', 'value');
-    }elsif(ref $value eq 'GADS::Schema::Result::Daterange') {
-        purge_date_range $value;
-    }elsif(ref $value eq 'GADS::Schema::Result::Enum') {
-        purge_value ($value, 'Enum', 'value');
-    }elsif(ref $value eq 'GADS::Schema::Result::File') {
-        purge_file $value;
-    }elsif(ref $value eq 'GADS::Schema::Result::Intgr') {
-        purge_value ($value, 'Intgr', 'value');
-    }elsif(ref $value eq 'GADS::Schema::Result::Person') {
-        purge_value($value, 'Person', 'value');
-    }elsif(ref $value eq 'GADS::Schema::Result::Ragval') {
-        purge_value ($value, 'Ragval', 'value');
-    }elsif(ref $value eq 'GADS::Schema::Result::String') {
-        purge_value ($value,'String', 'value');
-    }else{
-        say "Unknown type: " . ref $value;
-        exit(2);
-    }
-    say "Deleted $value";
-}
-
-sub purge_value {
-    my ($value, $value_source, $value_column) = @_;
-
-    schema->txn_do(sub {
-        my $source=schema->resultset($value_source)->find($value->id);
-        $source->update({$value_column => undef});
-    });
-}
-
-sub purge_calc {
-    my $calc = shift;
-    my @fields = ('value_text', 'value_int', 'value_date', 'value_numeric', 'value_datetime');
-
-    schema->txn_do(sub {
-        $calc->update({ $_ => undef }) foreach @fields;
-    });
-}
-
-sub purge_date_range {
-    my $daterange = shift;
-
-    schema->txn_do(sub {
-        $daterange->update({ from => undef, to => undef });
-    });
-}
-
 sub purge_file {
     my $file = shift;
 
-    schema->txn_do(sub {
-        my $value = $file->value;
-        $value->update({
-            name=>'purged',
-            mimetype=>'text/plain',
-            content=>encode('utf-8', 'purged'),
-        });
-    });
+    schema->txn_do(
+        sub {
+            my $value = $file->value;
+            $value->update(
+                {
+                    name     => 'purged',
+                    mimetype => 'text/plain',
+                    content  => encode( 'utf-8', 'purged' ),
+                }
+            );
+        }
+    );
+}
+
+sub purge {
+    my $value = shift;
+    if ( ref $value eq 'GADS::Schema::Result::File' ) {
+        purge_file $value;
+    }
+    else {
+        say "Unknown type: " . ref $value;
+        exit(2);
+    }
+    say "Deleted " . $value->id . "\n";
 }
 
 say "Usage: ./delete-file.pl <current_id> <layout_id> [<layout id>...]" unless @ARGV > 1;
@@ -90,21 +53,9 @@ my @records = $current->records->all;
 my @values;
 
 foreach my $layout (@ARGV) {
-    push @values, $_->calcvals->search({ layout_id => $layout })->all foreach @records;
-    push @values, $_->dateranges->search({ layout_id => $layout })->all foreach @records;
-    push @values, $_->dates->search({ layout_id => $layout })->all foreach @records;
-    push @values, $_->enums->search({ layout_id => $layout })->all foreach @records;
-    push @values, $_->files->search({ layout_id => $layout })->all foreach @records;
-    push @values, $_->intgrs->search({ layout_id => $layout })->all foreach @records;
-    push @values, $_->people->search({ layout_id => $layout })->all foreach @records;
-    push @values, $_->ragvals->search({ layout_id => $layout })->all foreach @records;
-    push @values, $_->strings->search({ layout_id => $layout })->all foreach @records;
+    push @values, $_->files->search( { layout_id => $layout } )->all foreach @records;
 }
 
-foreach my $value (@values) {
-    purge $value;
-    $total++;
-}
+purge $_ foreach @values;
 
-say "Purged $total records";
 exit(0);
