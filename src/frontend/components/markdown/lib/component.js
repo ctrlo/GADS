26ad1a77d4a94959745bf5642cc7515f53cc1964
Dave Roberts <d.roberts@ctrlo.com>
2024-06-06 15:50:04 +0100

    Further package removal

--- src/frontend/components/markdown/lib/component.js
+++ src/frontend/components/markdown/lib/component.js
@@ -1,5 +1,5 @@
 import { Component } from "component";
-import {Markdown} from "@davetheitguy/markdown-formatter";
+import MDC from "@davetheitguy/markdown-component";
 
 class MarkdownComponent extends Component {
   constructor(element) {
@@ -7,30 +7,12 @@ class MarkdownComponent extends Component {
     this.initMarkdownEditor();
   }
 
-  renderMarkdown(md) {
-    const mdEncoded = $('<span>').text(md.replace(/[\n\r]+/,"\n\n")).html()
-    return Markdown`${mdEncoded}`;
-  }
-
   initMarkdownEditor() {
 
     const $textArea = $(this.element).find(".js-markdown-input");
     const $preview = $(this.element).find(".js-markdown-preview");
-    $().on("ready", () => {
-      if ($textArea.val() !== "") {
-        const htmlText = this.renderMarkdown($textArea.val());
-        $preview.html(htmlText);
-      }
-    });
-    $textArea.keyup(() => {
-      const markdownText = $textArea.val();
-      if (!markdownText || markdownText === "") {
-        $preview.html('<p class="text-info">Nothing to preview!</p>');
-      } else {
-        const htmlText = this.renderMarkdown(markdownText);
-        $preview.html(htmlText);
-      }
-    });
+
+    new MDC($textArea, $preview);
   }
 }
 
