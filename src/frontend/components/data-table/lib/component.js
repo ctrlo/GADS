d94866fb508575e3f32b548f2e917f207eeae885
Dave Roberts <145559566+droberts-ctrlo@users.noreply.github.com>
2024-06-20 13:49:30 +0100

    Update component.js

--- src/frontend/components/data-table/lib/component.js
+++ src/frontend/components/data-table/lib/component.js
@@ -588,7 +588,7 @@ class DataTableComponent extends Component {
   getConf() {
     const confData = this.el.data('config')
     let conf = {}
-    
+
     if (typeof confData === 'string') {
       conf = JSON.parse(atob(confData))
     } else if (typeof confData === 'object') {
