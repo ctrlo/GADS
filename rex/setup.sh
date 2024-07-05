3dfbb1faab11d3d01a9d1a71ad5fe217c046006f
Dave Roberts <d.roberts@ctrlo.com>
2024-07-05 13:55:18 +0100

    New REX config, and updated README.md

---
+++ rex/setup.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+
+apt-get update
+apt-get install -y cpanminus gcc g++ make nano libaws-signature4-perl libdata-validate-ip-perl libdevel-caller-perl libhash-merge-perl 
+apt-get install -y libio-string-perl libsort-naturally-perl libterm-readkey-perl libtext-glob-perl liburi-perl libxml-simple-perl libyaml-perl
+apt-get install -y libversion-perl libfile-sharedir-install-perl libio-pty-perl libdigest-hmac-perl libnet-sftp-foreign-perl libnet-openssh-perl 
+apt-get install -y libjson-maybexs-perl libcpanel-json-xs-perl
+
+cpanm --notest Rex
