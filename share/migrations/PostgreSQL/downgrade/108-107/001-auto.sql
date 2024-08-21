f6d39dd3ad5c55d3b191515491efdfd41d1abf8c
Dave Roberts <145559566+droberts-ctrlo@users.noreply.github.com>
2024-08-21 11:42:23 +0100

    Add option to restrict reports to groups

---
+++ share/migrations/PostgreSQL/downgrade/108-107/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/droberts/source/gads2/bin/../share/migrations/_source/deploy/108/001-auto.yml' to '/home/droberts/source/gads2/bin/../share/migrations/_source/deploy/107/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+DROP TABLE report_group CASCADE;
+
+;
+
+COMMIT;
+
