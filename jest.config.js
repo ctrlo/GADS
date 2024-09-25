3200af92c29f7bc1d5eef5d3c22ae07567d22bc0
Dave Roberts <d.roberts@ctrlo.com>
2024-09-25 15:39:35 +0100

    Updated `tsconfig.json` and `jest.config.js`

--- jest.config.js
+++ jest.config.js
@@ -94,6 +94,7 @@ const config = {
     "^validation$": "<rootDir>/src/frontend/js/lib/validation",
     "^logging$": "<rootDir>/src/frontend/js/lib/logging",
     "^util/(.*)$": "<rootDir>/src/frontend/js/lib/util/$1",
+    "^testing$": "<rootDir>/src/frontend/testing/globals.definitions.ts",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
