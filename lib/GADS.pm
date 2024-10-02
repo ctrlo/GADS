3d1c3280a006cf8caaa1abc6a282662f9fd40fe5
Dave Roberts <d.roberts@ctrlo.com>
2024-10-02 16:40:29 +0100

    Moved code for post to correct method

--- lib/GADS.pm
+++ lib/GADS.pm
@@ -1696,10 +1696,6 @@ post '/file/:id?' => require_login sub {
     # File upload through the "manage files" interface
     if (my $upload = upload('file'))
     {
-        # Remove any invalid characters from the new name - this will possibly be changed to an error going forward
-        # Due to dragging allowing (almost) any character it is decided that this would be best so users can input what
-        # they want, and the text be stripped on rename server-side
-        $upload->filename =~ s/[^a-zA-Z0-9\._\-\(\)]//g;
         my $mimetype = $filecheck->check_file($upload); # Borks on invalid file type
         my $file;
         if (process( sub { $file = rset('Fileval')->create({
@@ -1797,6 +1793,10 @@ post '/api/file/?' => require_login sub {
 
     if (my $upload = upload('file'))
     {
+        # Remove any invalid characters from the new name - this will possibly be changed to an error going forward
+        # Due to dragging allowing (almost) any character it is decided that this would be best so users can input what
+        # they want, and the text be stripped on rename server-side
+        $upload->filename =~ s/[^a-zA-Z0-9\._\-\(\)]//g;
         my $mimetype = $filecheck->check_file($upload); # Borks on invalid file type
         my $filename = $upload->filename;
 
