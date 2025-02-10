f6c96295e8d4b3669ed80358403b2380a99f9d21
Dave Roberts <d.roberts@ctrlo.com>
2025-02-10 15:00:58 +0000

    Fixes for failing Cypress tests

--- cypress/e2e/functionality/02-homepage.cy.ts
+++ cypress/e2e/functionality/02-homepage.cy.ts
@@ -2,17 +2,14 @@ import { goodPassword, goodUser } from "../../support/constants";
 
 describe('Dashboard Tests', () => {
   const bigLipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque accumsan, sem et vulputate imperdiet, enim ipsum rhoncus massa, sit amet pellentesque lacus urna sit amet enim. In tristique mollis tincidunt. Sed eget ligula metus. Integer sodales placerat erat. Aliquam erat volutpat. Curabitur varius lacinia diam fringilla efficitur. Sed euismod purus vel turpis molestie, ac fringilla nisi vestibulum. Maecenas ullamcorper ornare dui sodales gravida. Donec maximus egestas eleifend. Etiam in ultrices ante. Duis quis volutpat turpis. Nulla dignissim ornare aliquet.
-Fusce interdum gravida est, sit amet vehicula nisi suscipit at. Pellentesque nec fermentum leo, in vehicula nulla. Nam dapibus ultricies tortor in maximus. Donec enim velit, molestie nec feugiat sed, posuere non ex. Nulla pellentesque gravida feugiat. Sed ornare purus vel libero semper aliquet. Nulla rutrum nunc sed vulputate gravida. Cras lobortis, lacus non tincidunt suscipit, leo quam vehicula libero, in vehicula diam justo ac est.
-Mauris tempus, mi nec sodales semper, metus neque blandit sem, non scelerisque nunc libero eu augue. Cras ornare ut lectus in mattis. Quisque magna elit, efficitur nec dolor sed, semper dictum nunc. Cras ultricies, augue eget interdum aliquam, quam ex blandit sem, nec sollicitudin ex elit non mauris. Fusce dui justo, feugiat id lacus sit amet, pulvinar tristique felis. Etiam rhoncus ex ut congue aliquet. Sed at felis eget neque rhoncus malesuada. Aliquam commodo condimentum massa, sed volutpat nibh congue et. Maecenas blandit massa sed nisl pulvinar, vitae consectetur tortor placerat. Nulla laoreet diam ipsum, sit amet consectetur sem condimentum quis. Nullam et justo sem. Sed et sapien tempus, scelerisque nisl ac, pretium arcu.
-Donec quis finibus ante. Nulla et dui posuere, semper elit quis, maximus ipsum. Aliquam ante nulla, pellentesque sed neque sit amet, finibus cursus est. Aliquam sed sollicitudin orci. Nulla malesuada augue lectus, ac tincidunt orci fermentum ac. Nullam pulvinar diam felis, sed condimentum arcu ornare sit amet. Praesent eget lobortis purus. In hac habitasse platea dictumst. Ut lorem nisl, fringilla vitae quam lobortis, vehicula egestas magna. Sed convallis placerat ante quis convallis. Vivamus pharetra quam diam, ut ultricies neque mollis vitae. Morbi augue tellus, feugiat a interdum a, tincidunt vel ante.
-Ut diam tortor, hendrerit eget ipsum non, suscipit mattis elit. Pellentesque ut porttitor risus, at pulvinar tortor. Integer eleifend volutpat efficitur. Maecenas massa odio, pharetra eu eleifend eu, volutpat sed dui. Donec efficitur sed risus sit amet imperdiet. Pellentesque nec arcu non nibh congue cursus et a sapien. Phasellus ullamcorper magna nec varius facilisis. Curabitur et tempus est. Nulla tincidunt porttitor mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean vitae pretium felis. Aliquam vehicula et nisl sit amet pharetra. Nulla ac sollicitudin velit. Cras egestas ac sem vitae sollicitudin. Nullam convallis risus id massa vestibulum imperdiet. Cras in enim sit amet ligula placerat eleifend.`
+Fusce interdum gravida est, sit amet vehicula nisi suscipit at. Pellentesque nec fermentum leo, in vehicula nulla. Nam dapibus ultricies tortor in maximus. Donec enim velit, molestie nec feugiat sed, posuere non ex. Nulla pellentesque gravida feugiat. Sed ornare purus vel libero semper aliquet. Nulla rutrum nunc sed vulputate gravida. Cras lobortis, lacus non tincidunt suscipit, leo quam vehicula libero, in vehicula diam justo ac est.`
 
   beforeEach(() => {
-    cy.loginAndGoTo(goodUser, goodPassword, 'http://localhost:3000/?did=1');
+    cy.loginAndGoTo(goodUser, goodPassword, 'http://localhost:3000/');
   })
 
   afterEach(() => {
-    cy.visit('http://localhost:3000/?did=1');
+    cy.visit('http://localhost:3000/');
     cy.location("pathname").should("not.include", "/login");
     cy.getByTitle("Logout")
       .should("exist")
@@ -21,54 +18,66 @@ Ut diam tortor, hendrerit eget ipsum non, suscipit mattis elit. Pellentesque ut 
   })
 
   it('Displays the home page correctly', () => {
-    cy.get("li.list__item").eq(0)
+    cy.get("div.nav-item").eq(0)
       .should("exist")
       .contains("Home dashboard (shared)")
-      .should("have.class", "link--active");
-    cy.get("li.list__item").eq(1)
+      .should("have.class", "active");
+    cy.get("div.nav-item").eq(1)
       .should("exist")
       .contains("Home dashboard (personal)")
-      .should("not.have.class", "link--active");
+      .should("not.have.class", "active");
   });
 
   it('Should navigate to the personal dashboard', () => {
-    cy.get("li.list__item").eq(1)
+    cy.get("div.nav-item").eq(1)
       .should("exist")
-      .should("not.have.class", "link--active")
+      .children("a")
+      .should("not.have.class", "active")
       .click();
+    cy.get("div.nav-item").eq(1)
+      .should("exist")
+      .children("a")
+      .should("have.class", "active");
   });
 
   it('Should navigate to the shared dashboard', () => {
-    cy.get("li.list__item").eq(0)
+    cy.get("div.nav-item").eq(0)
       .should("exist")
-      .should("not.have.class", "link--active")
+      .children("a")
+      .should("not.have.class", "active")
       .click();
+    cy.get("div.nav-item").eq(0)
+      .should("exist")
+      .children("a")
+      .should("have.class", "active");
   });
 
   context("Shared Dashboard", () => {
     it('Should cancel creation of a shared dashboard widget', () => {
+      cy.visit('http://localhost:3000/?did=1');
       cy.get(".ld-footer-container")
         .find("button")
         .eq(1)
         .click();
       cy.get(".ld-footer-container")
-        .find(".dropdown__menu")
+        .find(".dropdown-menu")
         .find("a")
-        .eq(1)
+        .eq(0)
         .click();
       cy.get("[aria-label='Edit Modal']").find("button.btn-cancel").click();
       cy.get(".ld-widget").should("have.length", 0);
     });
 
     it("Should create a shared dashboard widget", () => {
+      cy.visit('http://localhost:3000/?did=1');
       cy.get(".ld-footer-container")
         .find("button")
         .eq(1)
         .click();
       cy.get(".ld-footer-container")
-        .find(".dropdown__menu")
+        .find(".dropdown-menu")
         .find("a")
-        .eq(1)
+        .eq(0)
         .click();
       cy.get("[aria-label='Edit Modal']")
         .find("input[name='title']", { timeout: 10000 })
@@ -81,6 +90,7 @@ Ut diam tortor, hendrerit eget ipsum non, suscipit mattis elit. Pellentesque ut 
     });
 
     it("Should edit a shared dashboard widget", () => {
+      cy.visit('http://localhost:3000/?did=1');
       cy.get(".ld-widget").find(".ld-edit-button").click();
       cy.get("[aria-label='Edit Modal']")
         .find("input[name='title']", { timeout: 10000 })
@@ -95,6 +105,7 @@ Ut diam tortor, hendrerit eget ipsum non, suscipit mattis elit. Pellentesque ut 
     });
 
     it("Should edit a shared dashboard widget with a lot of text", () => {
+      cy.visit('http://localhost:3000/?did=1');
       cy.get(".ld-widget").find(".ld-edit-button").click();
       cy.get("[aria-label='Edit Modal']")
         .find("input[name='title']", { timeout: 10000 })
@@ -109,6 +120,7 @@ Ut diam tortor, hendrerit eget ipsum non, suscipit mattis elit. Pellentesque ut 
     });
 
     it("Should delete a shared dashboard widget", () => {
+      cy.visit('http://localhost:3000/?did=1');
       cy.get(".ld-widget").find(".ld-edit-button").click();
       cy.get("[aria-label='Edit Modal']")
         .find("input[name='title']", { timeout: 10000 })
@@ -120,30 +132,30 @@ Ut diam tortor, hendrerit eget ipsum non, suscipit mattis elit. Pellentesque ut 
 
   context("Personal Dashboard", () => {
     it('Should cancel creation of a personal dashboard widget', () => {
-      cy.get("a.link--primary").click();
+      cy.visit('http://localhost:3000/?did=2');
       cy.get(".ld-footer-container")
         .find("button")
         .eq(1)
         .click();
       cy.get(".ld-footer-container")
-        .find(".dropdown__menu")
+        .find(".dropdown-menu")
         .find("a")
-        .eq(1)
+        .eq(0)
         .click();
       cy.get("[aria-label='Edit Modal']").find("button.btn-cancel").click();
       cy.get(".ld-widget").should("have.length", 1);
     });
 
     it("Should create a personal dashboard widget", () => {
-      cy.get("a.link--primary").click();
+      cy.visit('http://localhost:3000/?did=2');
       cy.get(".ld-footer-container")
         .find("button")
         .eq(1)
         .click();
       cy.get(".ld-footer-container")
-        .find(".dropdown__menu")
+        .find(".dropdown-menu")
         .find("a")
-        .eq(1)
+        .eq(0)
         .click();
       cy.get("[aria-label='Edit Modal']")
         .find("input[name='title']", { timeout: 10000 })
@@ -156,7 +168,7 @@ Ut diam tortor, hendrerit eget ipsum non, suscipit mattis elit. Pellentesque ut 
     });
 
     it("Should edit a personal dashboard widget", () => {
-      cy.get("a.link--primary").click();
+      cy.visit('http://localhost:3000/?did=2');
       cy.get(".ld-widget").eq(1).find(".ld-edit-button").click();
       cy.get("[aria-label='Edit Modal']")
         .find("input[name='title']", { timeout: 10000 })
@@ -171,7 +183,7 @@ Ut diam tortor, hendrerit eget ipsum non, suscipit mattis elit. Pellentesque ut 
     });
 
     it("Should edit a personal dashboard widget with a lot of text", () => {
-      cy.get("a.link--primary").click();
+      cy.visit('http://localhost:3000/?did=2');
       cy.get(".ld-widget").eq(1).find(".ld-edit-button").click();
       cy.get("[aria-label='Edit Modal']")
         .find("input[name='title']", { timeout: 10000 })
@@ -186,7 +198,7 @@ Ut diam tortor, hendrerit eget ipsum non, suscipit mattis elit. Pellentesque ut 
     });
 
     it("Should delete a personal dashboard widget", () => {
-      cy.get("a.link--primary").click();
+      cy.visit('http://localhost:3000/?did=2');
       cy.get(".ld-widget").eq(1).find(".ld-edit-button").click();
       cy.get("[aria-label='Edit Modal']")
         .find("input[name='title']", { timeout: 10000 })
