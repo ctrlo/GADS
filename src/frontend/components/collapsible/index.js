51820e0e4e4f6d8b38d7bedcc0a89af84d234cf9
Dave Roberts <d.roberts@ctrlo.com>
2025-03-06 14:12:22 +0000

    Removed collapsible component as this can be handled by BS5

--- src/frontend/components/collapsible/index.js
+++
@@ -1,4 +0,0 @@
-import { initializeComponent } from 'component'
-import CollapsibleComponent from './lib/component'
-
-export default (scope) => initializeComponent(scope, '.collapsible', CollapsibleComponent)
