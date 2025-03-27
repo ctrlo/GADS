bd0df3275065722ca7be0a1d5fda1b144c5baa88
Dave Roberts <d.roberts@ctrlo.com>
2025-03-27 14:44:37 +0000

    Updated and formatted the bootstrap select common component

--- src/frontend/components/form-group/common/bootstrap-select.js
+++ src/frontend/components/form-group/common/bootstrap-select.js
@@ -1,50 +1,54 @@
-export const refreshSelects = (el)=>{
-    const ruleFilterSelects=[];
-    const operatorSelects=[];
+/**
+ * Refreshes the bootstrap select elements in the query builder with mapped events
+ * @param {JQuery<HTMLElement>} el The element hosting the boostrap select
+ */
+export const refreshSelects = (el) => {
+  const ruleFilterSelects = [];
+  const operatorSelects = [];
 
-    el.on("afterCreateRuleFilters.queryBuilder", (e, rule) => {
-      const ruleFilterSelect= $(rule.$el.find(`select[name=${rule.id}_filter]`));
-      if(!ruleFilterSelects.includes(ruleFilterSelect[0])) ruleFilterSelects.push(ruleFilterSelect[0]);
-      if(!ruleFilterSelect || !ruleFilterSelect[0]) {
-        console.error("No select found");
-        return;
-      }
-      ruleFilterSelect.data("live-search","true");
-      ruleFilterSelect.selectpicker();
-    });
+  el.on("afterCreateRuleFilters.queryBuilder", (e, rule) => {
+    const ruleFilterSelect = $(rule.$el.find(`select[name=${rule.id}_filter]`));
+    if (!ruleFilterSelects.includes(ruleFilterSelect[0])) ruleFilterSelects.push(ruleFilterSelect[0]);
+    if (!ruleFilterSelect || !ruleFilterSelect[0]) {
+      console.error("No select found");
+      return;
+    }
+    ruleFilterSelect.data("live-search", "true");
+    ruleFilterSelect.selectpicker();
+  });
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    el.on("afterCreateRuleOperators.queryBuilder", (e, rule, operators) => {
-      const operatorSelect = $(rule.$el.find(`select[name=${rule.id}_operator]`));
-      if(!operatorSelect || !operatorSelect[0]) {
-        console.error("No operator select found");
-        return;
-      }
-      if(!operatorSelects.includes(operatorSelect[0])) operatorSelects.push(operatorSelect[0]);
-      if(operatorSelect.data("live-search")) return;
-      operatorSelect.data("live-search","true");
-      operatorSelect.selectpicker();
-    });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  el.on("afterCreateRuleOperators.queryBuilder", (e, rule, operators) => {
+    const operatorSelect = $(rule.$el.find(`select[name=${rule.id}_operator]`));
+    if (!operatorSelect || !operatorSelect[0]) {
+      console.error("No operator select found");
+      return;
+    }
+    if (!operatorSelects.includes(operatorSelect[0])) operatorSelects.push(operatorSelect[0]);
+    if (operatorSelect.data("live-search")) return;
+    operatorSelect.data("live-search", "true");
+    operatorSelect.selectpicker();
+  });
 
-    el.on("afterSetRules.queryBuilder", () => {
-      for(const ruleFilterSelect of ruleFilterSelects) {
-        if(!ruleFilterSelect) {
-          continue;
-        }
-        $(ruleFilterSelect).selectpicker("refresh");
+  el.on("afterSetRules.queryBuilder", () => {
+    for (const ruleFilterSelect of ruleFilterSelects) {
+      if (!ruleFilterSelect) {
+        continue;
       }
-      for(const operatorSelect of operatorSelects) {
-        if(!operatorSelect) continue;
-        $(operatorSelect).selectpicker("refresh");
-      }
-    });
+      $(ruleFilterSelect).selectpicker("refresh");
+    }
+    for (const operatorSelect of operatorSelects) {
+      if (!operatorSelect) continue;
+      $(operatorSelect).selectpicker("refresh");
+    }
+  });
 
-    el.on("afterSetRuleOperator.queryBuilder", () => {
-      for(const operatorSelect of operatorSelects) {
-        if(!operatorSelect) {
-          continue;
-        }
-        $(operatorSelect).selectpicker("refresh");
+  el.on("afterSetRuleOperator.queryBuilder", () => {
+    for (const operatorSelect of operatorSelects) {
+      if (!operatorSelect) {
+        continue;
       }
-    });
+      $(operatorSelect).selectpicker("refresh");
+    }
+  });
 }
