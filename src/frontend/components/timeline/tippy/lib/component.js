4616a8fc6deae872b4e77126a3dd2ce7011bf47a
Dave Roberts <d.roberts@ctrlo.com>
2025-03-27 14:38:57 +0000

    Updated and formatted the timeline component

--- src/frontend/components/timeline/tippy/lib/component.js
+++ src/frontend/components/timeline/tippy/lib/component.js
@@ -1,18 +1,29 @@
 import { Component } from 'component'
 import tippy from 'tippy.js'
 
+/**
+ * Tippy Component
+ */
 class TippyComponent extends Component {
-  constructor(element)  {
-      super(element)
-      this.el = $(this.element)
+  /**
+   * Create a new Tippy Component
+   * @param {HTMLElement} element The element to attach the component to
+   */
+  constructor(element) {
+    super(element)
+    this.el = $(this.element)
   }
 
+  /**
+   * Initialize the tippy component
+   * @param {HTMLElement} wrapperEl The wrapper element to append the tippy to
+   */
   initTippy(wrapperEl) {
     tippy(this.element, {
       theme: 'light',
       allowHTML: true,
       interactive: true,
-      appendTo: wrapperEl,      
+      appendTo: wrapperEl,
     })
   }
 }
