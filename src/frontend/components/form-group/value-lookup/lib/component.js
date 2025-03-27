9f568394617ab68d80e5bd6d9ffcf749daab06a4
Dave Roberts <d.roberts@ctrlo.com>
2025-03-27 15:54:18 +0000

    Updated and formatted the value lookup component

--- src/frontend/components/form-group/value-lookup/lib/component.js
+++ src/frontend/components/form-group/value-lookup/lib/component.js
@@ -4,18 +4,28 @@ import { setFieldValues } from "set-field-values"
 
 /* Used to lookup a value at an external endpoint
  * and prefill other fields from the response */
-
 class ValueLookupComponent extends Component {
-  constructor(element)  {
+  /**
+   * Create a new value lookup component
+   * @param {HTMLElement} element The element to attach the component to
+   */
+  constructor(element) {
     super(element)
     this.initLookupComponent()
   }
 
+  /**
+   * Initialise the value lookup component
+   */
   initLookupComponent() {
     const field = this.getLookupEndpoint();
     this.setupValueLookup(field);
   }
 
+  /**
+   * Get the lookup endpoint
+   * @returns {Object} The lookup endpoint and fields
+   */
   getLookupEndpoint() {
     const lookup_endpoint = $(this.element).data("lookup-endpoint");
     const lookup_fields = $(this.element).data("lookup-fields");
@@ -26,18 +36,22 @@ class ValueLookupComponent extends Component {
     };
   }
 
+  /**
+   * Set up the value lookup for a field
+   * @param {*} field The field to setup the value lookup for
+   */
   setupValueLookup(field) {
     const endpoint = field.endpoint;
     const $field = field.field;
-    $field.on("change", function() {
+    $field.on("change", function () {
       const all_fields = []
       const data = {}
-      let has_values = field.fields.every(function(name_short){
-        const $f = $('.linkspace-field[data-name-short="'+name_short+'"]')
+      let has_values = field.fields.every(function (name_short) {
+        const $f = $('.linkspace-field[data-name-short="' + name_short + '"]')
         all_fields.push($f.data('name'))
         const values = getFieldValues($f)
         data[name_short] = values
-        return values.filter(function(value) { return value !== undefined }).length ? true : false
+        return values.filter(function (value) { return value !== undefined }).length ? true : false
       });
       // If one of the values in this group is blank then do not perform lookup
       if (!has_values) return false
@@ -53,34 +67,41 @@ class ValueLookupComponent extends Component {
         data: data,
         traditional: true, // Don't put stupid [] after the parameter keys
         dataType: 'json',
-      }).done(function(data){
+      }).done(function (data) {
         if (data.is_error || !data.result) {
           let error = data.message ? data.message : 'Unknown error'
           addStatusMessage($field, error, false, true)
         } else {
           for (const [name, value] of Object.entries(data.result)) {
-            var $f = $('.linkspace-field[data-name-short="'+name+'"]')
+            var $f = $('.linkspace-field[data-name-short="' + name + '"]')
             setFieldValues($f, value)
           }
           removeStatusMessage($field)
         }
       })
-      .fail(function(jqXHR, textStatus) {
-        // Use error in JSON from endpoint if available, otherwise try and
-        // interpret error response appropriately
-        const err_message = textStatus == "timeout"
-          ? `Failed to look up ${all_names}: request timed out`
-          : textStatus == "parsererror" // result not in JSON
-          ? `Failed to look up ${all_names}: unexpected response format from server`
-          : (jqXHR.responseJSON && jqXHR.responseJSON.message)
-          ? jqXHR.responseJSON.message
-          : `Failed to look up ${all_names}: ${jqXHR.statusText}`
-        addStatusMessage($field, err_message, false, true)
-      });
+        .fail(function (jqXHR, textStatus) {
+          // Use error in JSON from endpoint if available, otherwise try and
+          // interpret error response appropriately
+          const err_message = textStatus == "timeout"
+            ? `Failed to look up ${all_names}: request timed out`
+            : textStatus == "parsererror" // result not in JSON
+              ? `Failed to look up ${all_names}: unexpected response format from server`
+              : (jqXHR.responseJSON && jqXHR.responseJSON.message)
+                ? jqXHR.responseJSON.message
+                : `Failed to look up ${all_names}: ${jqXHR.statusText}`
+          addStatusMessage($field, err_message, false, true)
+        });
     });
   }
 }
 
+/**
+ * Add a status message
+ * @param {JQuery<HTMLElement>} $field The field to add the status message to
+ * @param {string} message The message to display
+ * @param {boolean} spinner Whether to show a spinner
+ * @param {boolean} is_error Whether the message is an error
+ */
 const addStatusMessage = ($field, message, spinner, is_error) => {
   let $notice = $field.find('.lookup-status')
   let $text = $notice.find('.status-text')
@@ -100,6 +121,10 @@ const addStatusMessage = ($field, message, spinner, is_error) => {
   $notice.removeClass("d-none")
 }
 
+/**
+ * Remove a status message
+ * @param {JQuery<HTMLElement>} $field The field to remove the status message from
+ */
 const removeStatusMessage = ($field) => {
   $field.find('.lookup-status').addClass("d-none")
 }
