ae06cbbcc2b8e0e6c6acfee27d9a85a3cd0a2214
Dave Roberts <d.roberts@ctrlo.com>
2025-03-27 14:31:54 +0000

    Updated and formatted the summernote component

--- src/frontend/components/summernote/lib/component.js
+++ src/frontend/components/summernote/lib/component.js
@@ -3,80 +3,95 @@ import { Component } from 'component'
 import 'summernote/dist/summernote-bs4'
 import { logging } from 'logging'
 
+/**
+ * SummerNote Component
+ */
 class SummerNoteComponent extends Component {
-    constructor(element)  {
-      super(element)
-      this.initSummerNote()
-    }
+  /**
+   * Create a new summer note component
+   * @param {HTMLElement} element The element to attach the summer note component to
+   */
+  constructor(element) {
+    super(element)
+    this.initSummerNote()
+  }
 
-    initSummerNote() {
-      const self = this;
-      $(this.element).summernote({
-        toolbar: [
-          ['style', ['style']],
-          ['font', ['bold', 'underline', 'clear']],
-          ['fontname', ['fontname']],
-          ['color', ['color']],
-          ['para', ['ul', 'ol', 'paragraph']],
-          ['table', ['table']],
-          ['insert', ['link', 'picture', 'video']],
-          ['view', ['codeview', 'help']],
-        ],
-        dialogsInBody: true,
-        height: 400,
-        callbacks: {
-          // Load initial content
-          onInit: function() {
-            const $sum_div = $(this);
-            const $sum_input = $sum_div.siblings('input[type=hidden].summernote_content')
-            $(this).summernote('code', $sum_input.val())
-          },
-          onImageUpload: function(files) {
-            for (var i = 0; i < files.length; i++) {
-              self.handleHtmlEditorFileUpload(files[i], this)
-            }
-          },
-          onChange: function(contents) {
-            const $sum_div = $(this).closest('.summernote')
-            // Ensure submitted content is empty string if blank content
-            // (easier checking for blank values)
-            if ($sum_div.summernote('isEmpty')) {
-              contents = ''
-            }
-            var $sum_input = $sum_div.siblings('input[type=hidden].summernote_content')
-            $sum_input.val(contents)
+  /**
+   * Initialize the summer note component
+   */
+  initSummerNote() {
+    const self = this;
+    $(this.element).summernote({
+      toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'underline', 'clear']],
+        ['fontname', ['fontname']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['codeview', 'help']],
+      ],
+      dialogsInBody: true,
+      height: 400,
+      callbacks: {
+        // Load initial content
+        onInit: function () {
+          const $sum_div = $(this);
+          const $sum_input = $sum_div.siblings('input[type=hidden].summernote_content')
+          $(this).summernote('code', $sum_input.val())
+        },
+        onImageUpload: function (files) {
+          for (var i = 0; i < files.length; i++) {
+            self.handleHtmlEditorFileUpload(files[i], this)
+          }
+        },
+        onChange: function (contents) {
+          const $sum_div = $(this).closest('.summernote')
+          // Ensure submitted content is empty string if blank content
+          // (easier checking for blank values)
+          if ($sum_div.summernote('isEmpty')) {
+            contents = ''
+          }
+          var $sum_input = $sum_div.siblings('input[type=hidden].summernote_content')
+          $sum_input.val(contents)
+        }
+      }
+    })
+  }
+
+  /**
+   * Handle file upload to embed in the editor
+   * @param {*} file The file to upload
+   * @param {*} el The element to upload the file to
+   */
+  handleHtmlEditorFileUpload(file, el) {
+    if (file.type.includes('image')) {
+      const data = new FormData()
+      data.append('file', file)
+      data.append('csrf_token', $('body').data('csrf'))
+      $.ajax({
+        url: '/file?ajax&is_independent',
+        type: 'POST',
+        contentType: false,
+        cache: false,
+        processData: false,
+        dataType: 'JSON',
+        data: data,
+        success: function (response) {
+          if (response.is_ok) {
+            $(el).summernote('editor.insertImage', response.url)
+          } else {
+            logging.error(response.error)
           }
         }
+      }).fail(function (e) {
+        logging.error(e)
       })
+    } else {
+      logging.error('The type of file uploaded was not an image')
     }
-
-    handleHtmlEditorFileUpload(file, el) {
-      if (file.type.includes('image')) {
-        const data = new FormData()
-        data.append('file', file)
-        data.append('csrf_token', $('body').data('csrf'))
-        $.ajax({
-          url: '/file?ajax&is_independent',
-          type: 'POST',
-          contentType: false,
-          cache: false,
-          processData: false,
-          dataType: 'JSON',
-          data: data,
-          success: function(response) {
-            if (response.is_ok) {
-              $(el).summernote('editor.insertImage', response.url)
-            } else {
-              logging.error(response.error)
-            }
-          }
-        }).fail(function(e) {
-          logging.error(e)
-        })
-      } else {
-        logging.error('The type of file uploaded was not an image')
-      }
-    }
+  }
 }
 
 export default SummerNoteComponent
