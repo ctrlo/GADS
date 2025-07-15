2133babd0985e013677bc6274835e03f72721ed2
Dave Roberts <d.roberts@ctrlo.com>
2025-07-15 11:45:47 +0100

    Removed excess files that weren't needed

--- src/frontend/js/lib/util/typeChecks/index.ts
+++
@@ -1,3 +0,0 @@
-export {isDefined} from './lib/generic';
-export { isFinite, isInteger, isNaN, isNotFinite, isNotInteger, isNotNaN, isNumber } from './lib/number';
-export { isEmptyString, isNotEmptyString, isString } from './lib/string';
