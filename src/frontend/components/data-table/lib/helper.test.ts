3195fc369db92d6bb9de12a41ee34272668bd687
Dave Roberts <d.roberts@ctrlo.com>
2025-07-16 14:35:48 +0100

    Formatted test and config code and updated test files
    Also removed extra yarn commands that weren't needed, and automated download of browserslist updates on build.

--- src/frontend/components/data-table/lib/helper.test.ts
+++ src/frontend/components/data-table/lib/helper.test.ts
@@ -4,6 +4,7 @@ import Datatable from 'datatables.net-bs4';
 
 describe('helper', () => {
     beforeEach(() => {
+        // create the DOM structure for the tests
         document.body.innerHTML = `
         <table id="target" class="table table-striped">
             <thead>
--- src/frontend/js/lib/util/filedrag/lib/filedrag.test.ts
+++ src/frontend/js/lib/util/filedrag/lib/filedrag.test.ts
@@ -2,6 +2,7 @@
 import { describe, it, expect, jest } from '@jest/globals';
 import FileDrag from './filedrag';
 
+// Test class implementation to expose private methods for testing
 class FileDragTest extends FileDrag {
     constructor(element: HTMLElement, onDrop: (files: File, index?: number, length?: number) => void = ()=>{}) {
         super(element, { debug: true }, onDrop);
