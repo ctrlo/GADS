8e80ddde31d0b231418c6a9f9c1fa61a639afbe2
Dave Roberts <145559566+droberts-ctrlo@users.noreply.github.com>
2025-07-28 11:57:36 +0100

    Update install_integration.sh

--- .github/workflows/install_integration.sh
+++ .github/workflows/install_integration.sh
@@ -5,8 +5,3 @@ apt-get install -y unzip gconf2 libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev 
 curl -o- https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
 && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
 apt-get update && apt-get install google-chrome-stable -y
-
-mkdir -p /chromedriver
-curl -o /chromedriver/chromedriver_linux64.zip "http://chromedriver.storage.googleapis.com/2.19/chromedriver_linux64.zip"
-unzip /chromedriver/chromedriver* -d /chromedriver
-ln /chromedriver/chromedriver /usr/local/bin/chromedriver
