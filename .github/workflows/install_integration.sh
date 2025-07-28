fbb2f0beeddf75ddd15d9afd51aa7d13bd830402
Dave Roberts <145559566+droberts-ctrlo@users.noreply.github.com>
2025-07-28 11:56:47 +0100

    Update install_integration.sh

--- .github/workflows/install_integration.sh
+++ .github/workflows/install_integration.sh
@@ -1,6 +1,5 @@
 apt-get update
-apt-get install -y unzip gconf2 libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb cpanminus libtest-simple-perl;
-cpanm --notest WebDriver::Tiny Test2::Tools::Compare;
+apt-get install -y unzip gconf2 libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb cpanminus libtest-simple-perl libwebdriver-tiny-perl;
 
 # We're running integration tests, so we need to set up browsers
 curl -o- https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
