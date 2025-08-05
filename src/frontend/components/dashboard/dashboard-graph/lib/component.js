48c7246496bac59cb8f69340f19d4e3f069ac034
Dave Roberts <d.roberts@ctrlo.com>
2025-08-05 15:19:29 +0100

    Updated dashboard and react components
    Updated formatting as this didn't update on prev commit properly

--- src/frontend/components/dashboard/dashboard-graph/lib/component.js
+++ src/frontend/components/dashboard/dashboard-graph/lib/component.js
@@ -2,21 +2,21 @@ import { do_plot_json } from '../../../graph/lib/chart';
 import GraphComponent from '../../../graph/lib/component';
 
 /**
- * DashboardGraphComponent class that initializes the dashboard graph and renders the graph using do_plot_json.
+ * Graph component for the dashboard
  */
 class DashboardGraphComponent extends GraphComponent {
     /**
-     * Create a DashboardGraphComponent instance.
-     * @param {HTMLElement} element The HTML element that this component will be attached to.
-     */
+   * Create a new dashboard graph component
+   * @param {HTMLElement} element The element to attach the component to
+   */
     constructor(element) {
         super(element);
         this.initDashboardGraph();
     }
 
     /**
-     * Initialize the dashboard graph by rendering the graph using do_plot_json.
-     */
+   * Initialize the dashboard graph
+   */
     initDashboardGraph() {
         const $graph = $(this.element);
         const graph_data = $graph.data('plot-data');
