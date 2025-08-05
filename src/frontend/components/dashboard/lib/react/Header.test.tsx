394db39a1dc5a8eb83cd822969b524df733ed61f
Dave Roberts <d.roberts@ctrlo.com>
2025-08-05 15:19:29 +0100

    Updated dashboard and react components
    Updated formatting as this didn't update on prev commit properly

--- src/frontend/components/dashboard/lib/react/Header.test.tsx
+++ src/frontend/components/dashboard/lib/react/Header.test.tsx
@@ -4,30 +4,32 @@ import '@testing-library/dom';
 import { describe, it, expect } from '@jest/globals';
 
 import Header from './Header';
+import { HeaderProps } from './types';
 
 describe('Header', () => {
-    it('should render the header', () => {
-        const props = {
+    it('Creates a header', () => {
+        const headerProps: HeaderProps = {
             hMargin: 0,
             dashboards: [
                 {
                     name: 'Dashboard 1',
-                    url: 'http://localhost:3000/dashboard1'
+                    url: 'http://localhost:3000/dashboard/1'
                 },
                 {
                     name: 'Dashboard 2',
-                    url: 'http://localhost:3000/dashboard2'
+                    url: 'http://localhost:3000/dashboard/2'
                 }
             ],
             currentDashboard: {
                 name: 'Dashboard 1',
-                url: 'http://localhost:3000/dashboard1'
+                url: 'http://localhost:3000/dashboard/1'
             },
-            includeH1: true,
-            loading: false
+            includeH1: true
         };
 
-        render(<Header {...props} />);
+        render(
+            <Header {...headerProps} />
+        );
 
         expect(screen.getByText('Dashboard 1').parentElement).toBeInstanceOf(HTMLHeadingElement);
         expect(screen.getByText('Dashboard 2').parentElement).toBeInstanceOf(HTMLAnchorElement);
