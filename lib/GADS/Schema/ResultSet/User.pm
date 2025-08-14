b94a48d637ce03cc6e866cd0a4c450f9a683becb
Timothy Legge <timlegge@gmail.com>
2025-08-13 21:30:57 -0300

    Fix authentication provider missing on create user wizard

--- lib/GADS/Schema/ResultSet/User.pm
+++ lib/GADS/Schema/ResultSet/User.pm
@@ -119,6 +119,7 @@ sub create_user
         resetpw               => $code,
         created               => DateTime->now,
         account_request_notes => $params{notes},
+        provider              => $params{provider},
     });
 
     my $audit = GADS::Audit->new(schema => $self->result_source->schema, user => $params{current_user});
