9123ada49ebead6dc462450963b9b9c4bbdb79a0
Dave Roberts <d.roberts@ctrlo.com>
2025-09-23 16:17:03 +0100

    Updated install scripts

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -5,21 +5,17 @@ apt-get install -y curl gpg
 curl -o- https://debian.ctrlo.com/repos/apt/debian/whatever.gpg.key | gpg --dearmor -o /usr/share/keyrings/ctrlo-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/ctrlo-keyring.gpg] https://debian.ctrlo.com/repos/apt/debian/ bookworm main' | tee /etc/apt/sources.list.d/ctrlo.list
 apt-get update
-apt-get install -y g++ gcc libalgorithm-dependency-perl libcgi-deurl-xs-perl libcrypt-urandom-perl
-apt-get install -y libctrlo-crypt-xkcdpassword-perl libctrlo-pdf-perl libdancer-plugin-auth-extensible-perl
-apt-get install -y libdancer2-perl libdancer2-plugin-auth-extensible-perl libdancer2-plugin-auth-extensible-provider-dbic-perl
-apt-get install -y libdancer2-plugin-dbic-perl libdata-compare-perl libdate-holidays-gb-perl libdatetime-event-random-perl
-apt-get install -y libdatetime-format-cldr-perl libdatetime-format-datemanip-perl libdatetime-format-dateparse-perl
-apt-get install -y libdatetime-format-iso8601-perl libdatetime-format-sqlite-perl libdatetime-format-strptime-perl
-apt-get install -y libdatetime-perl libdatetime-set-perl libdbd-pg-perl libdbix-class-helpers-perl
-apt-get install -y libdbix-class-migration-perl libdbix-class-perl libdbix-class-schema-loader-perl libfile-bom-perl
-apt-get install -y libfile-libmagic-perl libhtml-fromtext-perl libhtml-scrubber-perl libinline-lua-perl libjson-perl
-apt-get install -y liblist-compare-perl liblist-moreutils-perl liblog-log4perl-perl liblog-report-perl liblua5.3-dev
-apt-get install -y libmagic-dev libmail-message-perl libmail-transport-perl libmath-random-isaac-xs-perl libmath-round-perl
-apt-get install -y libmoox-singleton-perl libmoox-types-mooselike-datetime-perl libmoox-types-mooselike-perl
-apt-get install -y libnamespace-clean-perl libnet-oauth2-authorizationserver-perl libnet-saml2-perl libpdf-table-perl
-apt-get install -y libplack-perl libpng-dev libpq-dev libscalar-list-utils-perl libsession-token-perl libssl-dev
-apt-get install -y libstring-camelcase-perl libtest-mocktime-perl libtest-most-perl libtext-autoformat-perl
-apt-get install -y libtext-csv-encoded-perl libtext-markdown-perl libtie-cache-perl libtree-dagnode-perl liburl-encode-perl
-apt-get install -y libwww-form-urlencoded-xs-perl libwww-mechanize-chrome-perl libwww-mechanize-perl libyaml-perl postfix
-apt-get install -y postgresql-client starman perl libtest-tempdir-tiny-perl libtest-simple-perl
+apt-get install -y libapache2-mod-fastcgi libapache2-mod-perl2 libconfig-inifiles-perl libcrypt-saltedhash-perl libcrypt-urandom-perl libdancer2-perl
+apt-get install -y libdancer2-plugin-auth-extensible-provider-dbic-perl libdancer2-plugin-dbic-perl libdancer2-session-dbic-perl
+apt-get install -y libdata-dump-streamer-perl libdata-visitor-perl libdatetime-format-mysql-perl libdbd-mysql-perl libdbix-class-migration-perl
+apt-get install -y libdbix-class-perl libfcgi-perl libfile-copy-recursive-perl libio-all-perl liblog-report-lexicon-perl liblog-report-perl
+apt-get install -y libmail-box-perl libmail-transport-perl libmath-random-isaac-xs-perl libmoox-singleton-perl libpod-parser-perl libregexp-common-perl
+apt-get install -y libstring-camelcase-perl libtemplate-perl libtext-autoformat-perl libtext-csv-perl libyaml-libyaml-perl libdatetime-format-cldr-perl
+apt-get install -y libtree-dagnode-perl libalgorithm-dependency-perl libdatetime-set-perl libdata-compare-perl libdatetime-event-random-perl
+apt-get install -y libtext-csv-encoded-perl libhtml-fromtext-perl libhtml-scrubber-perl libdbd-pg-perl postgresql postgresql-contrib
+apt-get install -y libdatetime-format-pg-perl libset-infinite-perl libtie-cache-perl libdbix-class-helpers-perl libmath-round-perl
+apt-get install -y libmoox-types-mooselike-datetime-perl libdatetime-format-datemanip-perl libinline-lua-perl lua5.2 libctrlo-crypt-xkcdpassword-perl
+apt-get install -y libfile-slurp-perl libfile-mimeinfo-perl liblist-compare-perl libnet-oauth2-authorizationserver-perl libfontconfig1
+apt-get install -y libctrlo-pdf-perl libpdf-builder-perl fonts-liberation libdate-holidays-gb-perl libcgi-deurl-xs-perl libfile-bom-perl
+apt-get install -y libdatetime-format-iso8601-perl liblog-log4perl-perl libwww-mechanize-chrome-perl chromium libfile-libmagic-perl libnet-saml2-perl
+apt-get install -y liburl-encode-perl libtext-markdown-perl
--- .github/workflows/install_integration.sh
+++
@@ -1,7 +0,0 @@
-apt-get update
-apt-get install -y unzip gconf2 libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb cpanminus libtest-simple-perl libwebdriver-tiny-perl;
-
-# We're running integration tests, so we need to set up browsers
-curl -o- https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
-&& echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-apt-get update && apt-get install google-chrome-stable -y
