150b9dc10838256a1621e50fa5a3f6b05bcbfde9
Dave Roberts <d.roberts@ctrlo.com>
2025-10-30 16:03:32 +0000

    Updated loading of more less renderer with correct init

--- src/frontend/components/more-less/lib/MoreLessRenderer.ts
+++ src/frontend/components/more-less/lib/MoreLessRenderer.ts
@@ -1,10 +1,13 @@
+import { initializeComponent } from "component";
+
 export function RenderMoreLess<T extends HTMLElement = HTMLElement>(element: T, header: string = "Unknown") {
     const moreLessDiv = document.createElement('div');
     moreLessDiv.className = 'more-less';
     moreLessDiv.dataset.column = header;
     moreLessDiv.appendChild(element);
 
-    import("./component").then(({ default: MoreLess }) => new MoreLess(moreLessDiv));
+    // @ts-expect-error The component class has some odd TypeScript definitions
+    import("./component").then(({ default: MoreLess }) => initializeComponent(moreLessDiv, '.more-less', MoreLess));
 
     return moreLessDiv;
 }
