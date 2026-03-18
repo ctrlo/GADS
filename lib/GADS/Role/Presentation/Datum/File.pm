f1b1484d639e60ac396cd7998b3548bb1754d3c8
Dave Roberts <145559566+droberts-ctrlo@users.noreply.github.com>
2026-03-18 10:58:42 +0000

    Update File.pm

--- lib/GADS/Role/Presentation/Datum/File.pm
+++ lib/GADS/Role/Presentation/Datum/File.pm
@@ -7,7 +7,6 @@ sub presentation {
 
     my $base = $self->presentation_base;
 
-    $base->{purged} = $self->is_purged;
     $base->{files} = $self->files;
     $base->{purged} = $self->is_purged;
 
