95b1d30db793e5c3385eae21babbd3498032681a
Dave Roberts <145559566+droberts-ctrlo@users.noreply.github.com>
2026-04-29 16:41:04 +0100

    Js formatting (#612)
    * Formatted test and config code and updated test files
    
    Also removed extra yarn commands that weren't needed, and automated download of browserslist updates on build.
    
    * Updated filter where typeahead is missed
    
    * Update component.js
    
    * Reset erroneous changes
    
    * Updated error handler
    
    * Formatted test and config code and updated test files
    
    Also removed extra yarn commands that weren't needed, and automated download of browserslist updates on build.
    
    * Added editorconfig and upgraded ESLint to current version
    
    * Formatted and changed code with ESLint fixes - functionality has not changed
    
    Removed cjs from eslint - cjs are used for internal development files, and should never be included
    
    * Fixed formatting and code where this was broken on merge with current dev branch
    
    Updated files where dev changes weren't included

--- src/frontend/js/lib/util/storageProvider/lib/storageProvider.ts
+++ src/frontend/js/lib/util/storageProvider/lib/storageProvider.ts
@@ -13,23 +13,22 @@ type StringMap = { [key: string]: string };
  */
 class StorageProvider {
     /**
-     * Get the storage provider instance
-     * @returns {Storage | AppStorage} The storage provider instance
+     * Get the underlying storage provider
      */
     get provider() { return this.storage; }
 
     /**
      * Create a new StorageProvider instance
-     * @param {string} instance The instance name for the storage
-     * @param {Storage | AppStorage} storage The storage to use, defaults to gadsStorage
+     * @param instance The instance identifier for this storage instance
+     * @param storage The storage provider to use (defaults to a gadsStorage instance)
      */
     constructor(private readonly instance: string, private readonly storage: Storage | AppStorage = gadsStorage) {
     }
 
     /**
-     * Set an item in the storage
-     * @param {string} key The key to set the item for
-     * @param {string} value The value to set for the key
+     * Set an item within the storage provider
+     * @param key The key to use for the stored item
+     * @param value The value to use for the stored item
      */
     async setItem(key: string, value: string) {
         let item = await this.storage.getItem(this.instance);
