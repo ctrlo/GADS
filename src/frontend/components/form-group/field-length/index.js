73dc7c3f29595e1aebfa016769ff11c37dda29df
Dave Roberts <d.roberts@ctrlo.com>
2026-05-27 14:28:59 +0100

    Add field length control

---
+++ src/frontend/components/form-group/field-length/index.js
@@ -0,0 +1,6 @@
+import { initializeComponent } from "component"
+import FieldLengthComponent from "./lib/component"
+
+export default (scope) => {
+    initializeComponent(scope, "[data-max]", FieldLengthComponent);
+}
