147bd3cb4b86c4301cfec49713919732e7a28fef
Dave Roberts <d.roberts@ctrlo.com>
2026-06-18 12:03:48 +0000

    Fix erroneous changes

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const TerserPlugin = require('terser-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const { execSync } = require('child_process')
 
 const plugins = [
     new ProvidePlugin({
