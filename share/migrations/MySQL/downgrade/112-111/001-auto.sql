20b692a7b7ae1d9f07c76523baf88c7952d44a0e
Dave Roberts <d.roberts@ctrlo.com>
2026-08-27 15:21:03 +0000

    Update to increment schema version

---
+++ share/migrations/MySQL/downgrade/112-111/001-auto.sql
@@ -0,0 +1,17 @@
+-- Convert schema '/workspaces/GADS/bin/../share/migrations/_source/deploy/112/001-auto.yml' to '/workspaces/GADS/bin/../share/migrations/_source/deploy/111/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE calc_unique DROP CONSTRAINT calc_unique_ux_layout_datetime,
+                        DROP COLUMN value_datetime;
+
+;
+ALTER TABLE calcval DROP INDEX calcval_idx_value_datetime,
+                    DROP COLUMN value_datetime;
+
+;
+
+COMMIT;
+
