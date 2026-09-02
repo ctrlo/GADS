3184cb9607e604c3924360fa5d6e8f7677d6c5e5
Dave Roberts <d.roberts@ctrlo.com>
2026-09-02 14:15:38 +0000

    Remove unnecessary Cypress changes

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -18,7 +18,3 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-cy.on('window:confirm', () => {
-    return true; // Automatically confirm the confirmation dialog
-});
